1f3bddb6dedf6fc8a01d3819d249c79c07ae046f
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2002-07-25 16:00:00 +0000

    Added GPL license info to each sourcefile

--- kismet-devel/gpsdump.h
+++ kismet-devel/gpsdump.h
@@ -1,22 +1,22 @@
-// GPS dump file
+/*
+    This file is part of Kismet
 
-// File format:
-// FILE HEADER
-//   MAGIC
-//   VERSION
-//   TIME
-// NETWORK HEADERS [ x 254 elements ]
-//   NUMBER
-//   BSSID
-//   SSID
-// GPS DATA [ x number of packets ]
-//   NETWORK NUMBER
-//   LAT
-//   LON
-//   ALT
-//   SPD
-//   FIX
-//   TIME
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// GPS dump file
 
 #ifndef __GPSDUMP_H__
 #define __GPSDUMP_H__
