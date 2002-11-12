c0fc2395edee8bdae34e99c00fd4da5650187514
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2002-11-12 07:42:55 +0000

    Updated kismet_hopper, added ability to offset hop order to scan multiple channels at once

--- kismet_hopper.cc
+++ kismet_hopper.cc
@@ -33,6 +33,13 @@
 char *exec_name;
 #endif
 
+typedef struct capturesource {
+    char *interface;
+    int chanpos;
+    char *cardtype;
+    const char *cmd_template;
+};
+
 #define prism2 "wlanctl-ng %s lnxreq_wlansniff channel=%d enable=true"
 #define prism2_pcap "wlanctl-ng %s lnxreq_wlansniff channel=%d enable=true prismheader=true"
 #define prism2_bsd "prism2ctl %s -f %d"
@@ -101,8 +108,8 @@ int Usage(char *argv) {
     printf("Usage: %s [OPTION]\n", argv);
     printf(
            "  -f, --config-file <file>     Use alternate config file\n"
-           "  -i, --interface <if>         Interface to place in monitor mode (eth0, wlan0, etc)\n"
-           "  -t, --type <type>            Use alternate card type\n"
+           "  -d, --divide-channels        Divide channels across multiple cards when possible\n"
+           "  -c, --capture-source <src>   Packet capture source line (engine,interface,type,name)\n"
            "  -n, --international          Use international channels (1-14)\n"
            "  -s, --hopsequence            Use given hop sequence\n"
            "  -v, --velocity               Hopping velocity (hops per second)\n"
@@ -120,11 +127,7 @@ int main(int argc, char *argv[]) {
     int *chanlist;
     FILE *pgsock;
     char *configfile = NULL;
-    const char *interface = NULL;
     char *channame = "United States";
-    const char *type = NULL;
-    const char *cmd_template = NULL;
-    const char *label = NULL;
     struct stat fstat;
 
     FILE *pidfile;
@@ -143,18 +146,23 @@ int main(int argc, char *argv[]) {
 
     static struct option long_options[] = {   /* options table */
         { "config-file", required_argument, 0, 'f' },
-        { "international", no_argument, 0, 'n'},
-        { "interface", required_argument, 0, 'i' },
-        { "type", required_argument, 0, 't' },
+        { "international", no_argument, 0, 'n' },
+        { "capture-source", required_argument, 0, 'c' },
         { "hopsequence", required_argument, 0, 's' },
         { "velocity", required_argument, 0, 'v' },
+        { "divide-channels", no_argument, 0, 'd' },
+        { "help", no_argument, 0, 'h' },
         { 0, 0, 0, 0}
     };
+
+    vector<string> source_input_vec;
+    int divide = 0;
+    int divisions = 0;
 
     chanlist = us_channels;
     int option_index;
     while(1) {
-        int r = getopt_long(argc, argv, "f:ni:s:t:v:",
+        int r = getopt_long(argc, argv, "fnc:s:v:hd",
                             long_options, &option_index);
         if (r < 0) break;
 
@@ -165,12 +173,6 @@ int main(int argc, char *argv[]) {
         case 'n':
             chanlist = intl_channels;
 	    channame = "International";
-            break;
-        case 'i':
-            interface = optarg;
-            break;
-        case 't':
-            type = optarg;
             break;
         case 'v':
             if (sscanf(optarg, "%d", &freq) != 1) {
@@ -184,13 +186,21 @@ int main(int argc, char *argv[]) {
             if (!chanlist)
                 exit(1);
             break;
+        case 'c':
+            // Capture type
+            source_input_vec.push_back(optarg);
+            break;
+        case 'd':
+            // Divide channels
+            divide = 1;
+            break;
         default:
             Usage(argv[0]);
             break;
         }
     }
 
-    ConfigFile conf;
+    ConfigFile *conf = new ConfigFile;
 
     // If we haven't gotten a command line config option...
     if (configfile == NULL) {
@@ -200,52 +210,104 @@ int main(int argc, char *argv[]) {
 
     // Parse the config and load all the values from it and/or our command
     // line options.  This is a little soupy but it does the trick.
-    if (conf.ParseConfig(configfile) < 0) {
+    if (conf->ParseConfig(configfile) < 0) {
         exit(1);
     }
 
-    if (interface == NULL) {
-        if (conf.FetchOpt("capinterface") == "") {
-            fprintf(stderr, "FATAL:  No interface specified on the command line or in the config file.\n");
+    // Catch old configs and yell about them
+    if (conf->FetchOpt("cardtype") != "" || conf->FetchOpt("captype") != "" ||
+        conf->FetchOpt("capinterface") != "") {
+        fprintf(stderr, "FATAL:  Old config file options found.  To support multiple sources, Kismet now\n"
+                "uses a new config file format.  Please consult the example config file in your Kismet\n"
+                "source directory, OR do 'make forceinstall' and reconfigure Kismet.\n");
+        exit(1);
+    }
+
+    // Read all of our packet sources, tokenize the input and then start opening
+    // them.
+
+    vector<capturesource *> packet_sources;
+
+    string sourceopt;
+
+    // Read the config file if we didn't get any sources on the command line
+    if (source_input_vec.size() == 0) {
+        int sourcenum = 0;
+        char sourcestr[16];
+
+        while (snprintf(sourcestr, 16, "source%d", sourcenum++) &&
+               (sourceopt = conf->FetchOpt(sourcestr)) != "") {
+            source_input_vec.push_back(sourceopt);
+        }
+    }
+
+    if (source_input_vec.size() == 0) {
+        fprintf(stderr, "FATAL:  No valid packet sources defined in config or passed on command line.\n");
+        exit(1);
+    }
+
+    // Now tokenize the sources
+    for (unsigned int x = 0; x < source_input_vec.size(); x++) {
+        sourceopt = source_input_vec[x];
+
+        unsigned int begin = 0;
+        unsigned int end = sourceopt.find(",");
+        vector<string> optlist;
+
+        while (end != string::npos) {
+            string subopt = sourceopt.substr(begin, end-begin);
+            begin = end+1;
+            end = sourceopt.find(",", begin);
+            optlist.push_back(subopt);
+        }
+        optlist.push_back(sourceopt.substr(begin, sourceopt.size() - begin));
+
+        if (optlist.size() < 4) {
+            fprintf(stderr, "FATAL:  Invalid source line '%s'\n", sourceopt.c_str());
             exit(1);
         }
 
-        interface = conf.FetchOpt("capinterface").c_str();
+        capturesource *newsource = new capturesource;
+        newsource->interface = strdup(optlist[1].c_str());
+        newsource->cardtype = strdup(optlist[2].c_str());
+        newsource->chanpos = 0;
+
+        packet_sources.push_back(newsource);
     }
 
-    if (type == NULL) {
-        if (conf.FetchOpt("cardtype") == "") {
-            fprintf(stderr, "FATAL:  No card type specified on the command line or in the config file.\n");
+    for (unsigned int src = 0; src < packet_sources.size(); src++) {
+        char *type = packet_sources[src]->cardtype;
+
+        if ((!strcasecmp(type, "cisco") || !strcasecmp(type, "cisco_bsd")) &&
+            packet_sources.size() == 1) {
+            fprintf(stderr, "FATAL:  Cisco cards don't need to channel hop.\n");
+            exit(1);
+        } else if (!strcasecmp(type, "prism2")) {
+            packet_sources[src]->cmd_template = prism2;
+            divisions++;
+        } else if (!strcasecmp(type, "prism2_pcap")) {
+            packet_sources[src]->cmd_template = prism2_pcap;
+            divisions++;
+        } else if (!strcasecmp(type, "prism2_bsd")) {
+            packet_sources[src]->cmd_template = prism2_bsd;
+            divisions++;
+        } else if (!strcasecmp(type, "prism2_hostap")) {
+            packet_sources[src]->cmd_template = prism2_hostap;
+            divisions++;
+        } else if (!strcasecmp(type, "orinoco")) {
+            packet_sources[src]->cmd_template = orinoco;
+            divisions++;
+        } else {
+            fprintf(stderr, "FATAL: Source %d: Unknown card type '%s'.\n", src, type);
+            exit(1);
         }
 
-        type = conf.FetchOpt("cardtype").c_str();
+        free(type);
     }
 
-    if (!strcasecmp(type, "cisco") || !strcasecmp("cardtype", "cisco_bsd")) {
-        fprintf(stderr, "FATAL:  Cisco cards don't need to channel hop.\n");
-        exit(1);
-    } else if (!strcasecmp(type, "prism2")) {
-        label = "prism2";
-        cmd_template = prism2;
-    } else if (!strcasecmp(type, "prism2_pcap")) {
-        label = "prism2 pcap";
-        cmd_template = prism2_pcap;
-    } else if (!strcasecmp(type, "prism2_bsd")) {
-        label = "prism2 BSD";
-        cmd_template = prism2_bsd;
-    } else if (!strcasecmp(type, "prism2_hostap")) {
-        label = "prism2 hostap";
-	cmd_template = prism2_hostap;
-    } else if (!strcasecmp(type, "orinoco")) {
-        label = "orinoco";
-        cmd_template = orinoco;
-    } else if (!strcasecmp(type, "orinoco_bsd")) {
-        fprintf(stderr, "FATAL:  No monitor/hopper code for Orinoco on BSD yet.\n");
-        exit(1);
-    } else {
-        fprintf(stderr, "FATAL:  Unknown card type '%s'.\n", type);
-        exit(1);
-    }
+    // Free up the memory
+    delete conf;
+
 
     if ((pidfile = fopen(pidpath, "w")) == NULL) {
         fprintf(stderr, "FATAL:  Could not open PID file '%s' for writing.\n", pidpath);
@@ -277,11 +339,20 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Hopping %d channel%sper second (%ld microseconds per channel)\n",
             freq, freq > 1 ? "s " : " ", interval);
 
+    if (divide && divisions > 1) {
+        int nchannels = 0;
+        while (chanlist[nchannels++] != -1) ;
+        fprintf(stderr, "Dividing %d channels into %d segments.\n", nchannels, divisions);
+        for (unsigned int ofst = 0; ofst < packet_sources.size(); ofst++) {
+            packet_sources[ofst]->chanpos = (nchannels / divisions) * ofst;
+            printf("Source %d starting at offset %d\n", ofst, packet_sources[ofst]->chanpos);
+        }
+    }
 
-    fprintf(stderr, "%s - Channel hopping (%s) on interface %s as a %s card.\n",
-            argv[0], channame, interface, label);
+    for (unsigned int src = 0; src < packet_sources.size(); src++)
+        fprintf(stderr, "%s - Source %d - Channel hopping (%s) on interface %s\n",
+                argv[0], src, channame, packet_sources[src]->interface);
 
-    int chpos = 0;
     char cmd[1024];
     int statime = 0;
     while (1) {
@@ -293,18 +364,20 @@ int main(int argc, char *argv[]) {
             }
         }
 
+        for (unsigned int src = 0; src < packet_sources.size(); src++) {
+            capturesource *csrc = packet_sources[src];
+            snprintf(cmd, 1024, csrc->cmd_template, csrc->interface, chanlist[csrc->chanpos++]);
 
-        snprintf(cmd, 1024, cmd_template, interface, chanlist[chpos++]);
+            if (chanlist[csrc->chanpos] == -1)
+                csrc->chanpos = 0;
 
-        if (chanlist[chpos] == -1)
-            chpos = 0;
+            if ((pgsock = popen(cmd, "r")) < 0) {
+                fprintf(stderr, "Could not popen ``%s''.  Aborting.\n", cmd);
+                exit(0);
+            }
 
-        if ((pgsock = popen(cmd, "r")) < 0) {
-            fprintf(stderr, "Could not popen ``%s''.  Aborting.\n", cmd);
-            exit(0);
+            pclose(pgsock);
         }
-
-        pclose(pgsock);
 
         usleep(interval);
 
