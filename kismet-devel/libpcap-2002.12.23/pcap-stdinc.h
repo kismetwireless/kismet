302007f2c96dc642c396711e87333153060ddd48
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2002-12-23 18:06:51 +0000

    Trying to fix cvs

---
+++ kismet-devel/libpcap-2002.12.23/pcap-stdinc.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2002
+ *	Politecnico di Torino.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source code distributions
+ * retain the above copyright notice and this paragraph in its entirety, (2)
+ * distributions including binary code include the above copyright notice and
+ * this paragraph in its entirety in the documentation or other materials
+ * provided with the distribution, and (3) all advertising materials mentioning
+ * features or use of this software display the following acknowledgement:
+ * ``This product includes software developed by the Politecnico
+ * di Torino, and its contributors.'' Neither the name of
+ * the University nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior
+ * written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @(#) $Header: /home/dragorn/src/CVS/kismet/kismet-devel/libpcap-2002.12.23/Attic/pcap-stdinc.h,v 1.1 2002/12/23 18:06:51 dragorn Exp $ (LBL)
+ */
+
+#define SIZEOF_CHAR 1
+#define SIZEOF_SHORT 2
+#define SIZEOF_INT 4
+
+#define _WINSOCKAPI_
+#include <fcntl.h>
+
+#include <winsock2.h>
+#include "bittypes.h"
+#include <time.h>
+#include <io.h>
+
+#ifndef __MINGW32__
+#include "IP6_misc.h"
+#endif
+
+#define caddr_t char*
+
+#define snprintf _snprintf
+#define vsnprintf _vsnprintf
+#define inline __inline 
