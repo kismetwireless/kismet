b0830662edc3a465caaa7c57368383c7b9141f73
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2004-06-06 04:33:23 +0000

    Merged patch from Michael Scherer to fix gcc3.4 compiling

--- kismet-devel/packet.h
+++ kismet-devel/packet.h
@@ -550,7 +550,7 @@ public:
     // This isn't quite like STL iterators, because I'm too damned lazy to deal with all
     // the nasty STL hoop-jumping.  This does provide a somewhat-stl-ish interface to
     // iterating through the singleton and masked maps
-    friend class iterator {
+    class iterator {
         friend class macmap;
 
     public:
