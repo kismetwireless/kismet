ccaa65030152b1148de88885902ed1b57b49e39a
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2005-05-30 19:32:16 +0000

    Fixed stupid inversion of flite test

--- kismet_client.cc
+++ kismet_client.cc
@@ -278,7 +278,7 @@ void SpeechHandler(int *fds, const char *player) {
                 char spk_call[1024];
                 snprintf(spk_call, 1024, "echo \"(%s\\\"%s\\\")\" | %s "
 						 ">/dev/null 2>/dev/null",
-						 flite ? "SayText " : "", data, player);
+						 flite ? "": "SayText ", data, player);
 
                 system(spk_call);
 
