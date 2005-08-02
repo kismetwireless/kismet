6b4dfe8692355c92af3148ba389f29c475d83264
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2005-08-02 02:16:43 +0000

    Tiny tweak to munger to handle ' ' and '~' (edge cases) properly instead of  escaping them

--- packet.cc
+++ packet.cc
@@ -87,7 +87,7 @@ void MungeToPrintable(char *in_data, int max) {
 	int i;
 
 	for (i = 0; i < max; i++) {
-		if ((unsigned char) in_data[i] > 32 && (unsigned char) in_data[i] < 176) {
+		if ((unsigned char) in_data[i] >= 32 && (unsigned char) in_data[i] <= 176) {
 			ret += in_data[i];
 		} else {
 			ret += '\\';
