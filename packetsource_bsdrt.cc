41af38e89c6704a0f99ce4f235af4f576455f4ab
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2007-07-02 19:47:23 +0000

    Merged BSD patch from Anthony Martinez

--- packetsource_bsdrt.cc
+++ packetsource_bsdrt.cc
@@ -18,6 +18,15 @@
 
 #include "config.h"
 
+#ifdef SYS_NETBSD
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <net/if_media.h>
+#include <net80211/ieee80211.h>
+#include <net80211/ieee80211_ioctl.h>
+#endif
+
 #if (defined(HAVE_LIBPCAP) && (defined(SYS_OPENBSD) || defined(SYS_NETBSD) || \
 							   defined(SYS_FREEBSD)))
 
