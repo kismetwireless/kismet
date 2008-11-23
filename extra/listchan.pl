eadba22da52c95754df8de53e2b633d3ba2adf8c
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2008-11-23 08:10:19 +0000

    Merged updated listchan.pl from Tom Molin

--- extra/listchan.pl
+++ extra/listchan.pl
@@ -1,18 +1,37 @@
 #!/usr/bin/perl
 
-$device = shift;
+my @devices = `cat /proc/net/wireless`;
+my $nodevs = "0";
 
-die("No device specified") if ($device eq "");
-
-for ($i = 0; $i < 255; $i++)
+foreach my $dev (@devices)
 {
-	undef $bad;
-	open (IWCONFIG, "iwconfig 2>&1 $device channel $i|");
-	while (<IWCONFIG>)
-	{ /Error for wireless request "Set Frequency"/ && ($bad++); }
-	next if $bad;
-	$list .= "$i,";
+	if ($dev !~ /(^Inter-)|(^ face)/)
+	{
+		$nodevs++;	
+		$dev =~ s/^(.+)://;
+		$dev = $1;
+		$dev =~ s/^ //;
+		checkchans ($dev);
+	}
 }
-$list =~ s/,$//;
-print "$list\n";
+
+
+sub checkchans
+{
+	my $device = $_[0];
+	die("No device specified") if ($device eq "");
+	for ($i = 0; $i < 255; $i++)
+	{
+		undef $bad;
+		open (IWCONFIG, "iwconfig 2>&1 $device channel $i|");
+		while (<IWCONFIG>)
+		{ /Error for wireless request "Set Frequency"/ && ($bad++); }
+		next if $bad;
+		$list .= "$i,";
+	}
+	$list =~ s/,$//;
+	print "Channels supported by $device: $list\n";
+}
+
+
 
