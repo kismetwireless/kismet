6ceaa5280a0a0798082aeb701891db6fdd939ed1
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2010-06-10 20:44:32 +0000

    Started ruby interface + examples

---
+++ ruby/kismet.rb
@@ -0,0 +1,169 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'time'
+
+class Kismet
+	def initialize(host = "localhost", port = 2501)
+		@port = port
+		@host = host
+		@die = 0
+		@cmd = 0
+		@callbacks = { }
+		@ackbacks = { }
+	end
+
+	def connect()
+		@die = 0
+		begin 
+			@conn = TCPSocket.new(@host, @port)
+		rescue
+			puts "error: #{$!}"
+		end
+	end
+
+	def run()
+		@thr = Thread.new() {
+			while 1
+				begin
+					#print @conn.gets() + "\n"
+					parseline @conn.gets()
+				rescue
+					break if @die
+
+					puts "read error: #{$!}"
+					break
+				end
+			end
+		}
+	end
+
+	def sendraw(txd)
+		begin
+			@conn.puts(txd)
+		rescue
+			break if @die
+
+			puts "write error: #{$!}"
+		end
+	end
+
+	def kill()
+		@die = 1
+		@conn.close
+		@thr.join
+	end
+
+	def wait()
+		@thr.join
+	end
+
+	def parseline(line)
+		re = Regexp.new(/\*([A-Z0-9]+): (.*)\n/)
+		
+		md = re.match(line)
+		
+		exit if md == nil
+		
+		#puts md.length
+		
+		#for ss in 1..md.length
+		#	puts md[ss]
+		#end
+		
+		return if md.length != 3
+
+		p = md[1].upcase
+
+		if p == "ACK"
+			f = parsedata(["cmdid", "text"], md[2])
+
+			if @ackbacks[f['cmdid'].to_i] != nil
+				send(@ackbacks[f['cmdid'].to_i], f['text'])
+				@ackbacks[f['cmdid'].to_i] = nil
+			end
+			
+			return p
+		end
+
+		if @callbacks[p] != nil
+			f = parsedata(@callbacks[p][0], md[2])
+			#puts "#{p} got #{f.length} fields"
+			send(@callbacks[p][1], p, f)
+		end
+
+		return md[1]
+	end
+
+	def parsedata(fields, data)
+		in_delim = 0
+		pos = 0
+
+		da = {}
+		f = ""
+		fnum = 0
+
+		for pos in 0..data.length - 1
+			if data[pos, 1] == "\001" and in_delim == 1
+				in_delim = 0
+				next
+			elsif data[pos, 1] == "\001" and in_delim == 0
+				in_delim = 1
+				next
+			elsif data[pos, 1] == ' ' and in_delim == 0
+				#puts "#{fields[fnum]} #{f}"
+				da[fields[fnum]] = f
+				fnum = fnum + 1
+				f = ""
+				next
+			else 
+				f << data[pos, 1]
+			end
+
+			# printf "%c", data[pos]
+		end
+
+		#puts "#{fields[fnum]} #{f}"
+		da[fields[fnum]] = f
+
+		return da
+	end
+
+	def subscribe(proto, fields, hndl, ackback = nil)
+		p = proto.upcase
+
+		# puts "subscribe #{p} #{fields}"
+
+		if @callbacks[p] != nil
+			puts "!!! #{p} already declared with fields #{@callbacks[p][0]}, replacing"
+		end
+
+		@callbacks[p] = [ fields, hndl ]
+
+		fe = ""
+
+		fields.each { |f| fe << "#{f}," }
+
+		if ackback != nil 
+			@ackbacks[@cmd] = ackback
+		end
+
+		sendraw("!#{@cmd} ENABLE #{p} #{fe}\n")
+
+		@cmd = @cmd + 1
+		
+	end
+
+	def unsubscribe(proto)
+		p = proto.upcase
+		
+		@callbacks[p] = nil
+
+		sendraw("!0 REMOVE #{p}")
+	end
+
+end
+
+def genericcb(proto, fields)
+	puts "Proto #{proto} numf #{fields.length}"
+end
---
+++ ruby/kismet_list.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'time'
+require 'kismet'
+
+def bssidcb(proto, fields)
+	puts "Kismet saw network #{fields['bssid']} manuf #{fields['manuf']} on channel #{fields['channel']}"
+end
+
+def bssiddiecb(text)
+	puts "BSSID ack"
+	$k.kill
+	exit
+end
+
+$k = Kismet.new()
+$k.connect()
+
+$k.run()
+
+$k.subscribe("bssid", ["bssid", "manuf", "channel"], :bssidcb, :bssiddiecb)
+
+$k.wait()
