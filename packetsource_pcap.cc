7c9687545595663471d46d6f4a5848358913dc8c
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2012-01-17 00:29:37 +0000

    More pcap error handling

--- packetsource_pcap.cc
+++ packetsource_pcap.cc
@@ -106,7 +106,7 @@ int PacketSource_Pcap::OpenSource() {
 
 	free(unconst);
 
-	if (strlen(errstr) > 0) {
+	if (strlen(errstr) > 0 || pd == NULL) {
 		globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
 		return 0;
 	}
@@ -154,7 +154,10 @@ int PacketSource_Pcap::OpenSource() {
 
 	if (strlen(errstr) > 0) {
 		globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
-		pcap_close(pd);
+
+		if (pd != NULL)
+			pcap_close(pd);
+
 		return 0;
 	}
 
