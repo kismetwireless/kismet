79296d9f5ba077d66bc5f1704e12c85bf4393aa3
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2012-10-01 10:45:53 -0400

    Added licensing info on Ruby files

--- ruby/kismet_shootout.rb
+++ ruby/kismet_shootout.rb
@@ -1,8 +1,23 @@
 #!/usr/bin/env ruby
 
-# Very basic example for logging Kismet data to SQLite
-# Would need to be expanded for more fields and better logging,
-# contributions happily accepted
+# Moderately complex example of using Rub with Kismet, compares the relative 
+# performance of multiple wifi cards on a Kismet server
+
+#   This file is part of Kismet
+#
+#   Kismet is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   Kismet is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Kismet; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 require 'socket'
 require 'time'
