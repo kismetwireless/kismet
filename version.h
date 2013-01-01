80a8efac160b4b4cf0e47fcafb505264d3a6aff5
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2013-01-01 02:49:35 -0500

    Change version note to git
    Conflicts:
    	version.h

--- version.h
+++ version.h
@@ -19,9 +19,9 @@
 #ifndef __KISVERSION_H__
 #define __KISVERSION_H__
 
-#define VERSION_MAJOR	"2011"
-#define VERSION_MINOR	"03"
-#define VERSION_TINY	"R2"
+#define VERSION_MAJOR	"2013"
+#define VERSION_MINOR	"GIT"
+#define VERSION_TINY	"R0"
 
 #define REVISION		"$Rev$"
 #define REVDATE			"$LastChangedDate$"
