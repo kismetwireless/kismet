3e53ce379a8855314f69f5d8c42bbbb1a39fcaa2
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2013-03-13 15:40:16 -0400

    Backport b2bc90ae7104d7a16ec362245b6e30140e9cb518 	Fix menu accelerator

--- kis_panel_widgets.cc
+++ kis_panel_widgets.cc
@@ -952,6 +952,18 @@ int Kis_Menu::AddMenuItem(string in_text, int menuid, char extra) {
 	item->auxptr = NULL;
 	item->checksymbol = 'X';
 
+	if (extra != 0) {
+		for (vector<Kis_Menu::_menuitem *>::iterator p = 
+			 menubar[menuid]->items.begin(); p != menubar[menuid]->items.end(); p++) {
+			if ((*p)->extrachar == extra) {
+				_MSG("New menu item '" + in_text + "' shortcut '" + extra + "' "
+					 "conflicts with existing item '" + (*p)->text + "'",
+					 MSGFLAG_ERROR);
+				item->extrachar = 0;
+			}
+		}
+	}
+
 	// Auto-disable spacers
 	if (item->text[0] != '-')
 		item->enabled = 1;
@@ -1571,9 +1583,10 @@ int Kis_Menu::KeyPress(int in_key) {
 					int ret = (cur_menu * 100) + x + 1;
 
 					// Per-menu callbacks
-					if (menubar[cur_menu]->items[cur_item]->callback != NULL) 
-						(*(menubar[cur_menu]->items[cur_item]->callback))
-							(globalreg, ret, menubar[cur_menu]->items[cur_item]->auxptr);
+					if (menubar[cur_menu]->items[x]->callback != NULL) {
+						(*(menubar[cur_menu]->items[x]->callback))
+							(globalreg, ret, menubar[cur_menu]->items[x]->auxptr);
+					}
 	
 					// Widget-wide callbacks
 					if (cb_activate != NULL) 
