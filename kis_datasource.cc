6531d504c559a0b8d9b975e3c34a374e328d8ecf
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2016-04-06 19:30:32 -0400

    Fix file names

--- kis_datasource.cc
+++ kis_datasource.cc
@@ -16,7 +16,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "kisdatasource.h"
+#include "kis_datasource.h"
 #include "simple_datasource_proto.h"
 #include "endian_magic.h"
 
