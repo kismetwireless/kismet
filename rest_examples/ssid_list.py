27ddbe2b17c4b71980a5b3337f20227ef36db424
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2016-04-10 17:17:38 -0400

    Converted ssid list example to use rest helper class

--- rest_examples/ssid_list.py
+++ rest_examples/ssid_list.py
@@ -1,53 +1,31 @@
 #!/usr/bin/env python
 
-import msgpack, urllib, sys
+import sys, KismetRest
 
 if len(sys.argv) < 2:
     print "Expected server URI"
     sys.exit(1)
 
-try:
-    packbin = urllib.urlopen("%s/devices/all_devices.msgpack" % sys.argv[1]).read()
-except Exception as e:
-    print "Failed to get data from server: ", e
-    sys.exit(1)
+kr = KismetRest.Kismet(sys.argv[1])
 
-try:
-    summary_data = msgpack.unpackb(packbin)[1]
-except:
-    print "Something went wrong unpacking the data"
-    print "Sorry."
-    sys.exit()
+# Get summary of devices
+devices = kr.DeviceSummary()
 
-for di in summary_data:
-    try:
-        d = di[1]
-    except Exception as e:
-        print "Couldn't get device sub-rec:", e
-
-    try:
-        if not d['kismet.device.base.phyname'][1] == "IEEE802.11":
-            continue
-    except:
-        print "Could not check phy"
+# Print the SSID for every device we can.  Stupid print; no comparison
+# of the phy type, no handling empty ssid, etc.
+for d in devices:
+    if not d['kismet.device.base.phyname'] == "IEEE802.11":
         continue
 
-    try:
-        ssidbin = urllib.urlopen("%s/devices/%s.msgpack/dot11.device/dot11.device.last_beaconed_ssid" % (sys.argv[1], d['kismet.device.base.key'][1])).read()
-    except Exception as e:
-        continue
-
-    try:
-        ssid = msgpack.unpackb(ssidbin)[1]
-    except Exception as e:
-        continue
+    k = d['kismet.device.base.key']
+    ssid = kr.DeviceField(k, "dot11.device/dot11.device.last_beaconed_ssid")
 
     if ssid == "":
-        ssid = "<unknown>"
+        continue
 
-    print "MAC", d['kismet.device.base.macaddr'][1][0],
-    print "Type", d['kismet.device.base.type'][1],
-    print "Channel",d['kismet.device.base.channel'][1],
+    print "MAC", d['kismet.device.base.macaddr'],
+    print "Type", d['kismet.device.base.type'],
+    print "Channel",d['kismet.device.base.channel'],
     print "SSID", ssid
 
 
