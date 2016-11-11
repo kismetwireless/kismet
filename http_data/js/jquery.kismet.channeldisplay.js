90dd0b34bf7cbc1ad697f35e330920b933b7f553
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2016-11-11 16:52:14 -0500

    Initial time-based line graph of devices

--- http_data/js/jquery.kismet.channeldisplay.js
+++ http_data/js/jquery.kismet.channeldisplay.js
@@ -19,7 +19,13 @@
     var timerid = -1;
 
     var devgraph_chart = null;
+    var timegraph_chart = null;
+
+    var devgraph_canvas = null;
+    var timegraph_canvas = null;
+
     var picker = null;
+    var graphtype = null;
     var coming_soon = null;
 
     var visible = false;
@@ -36,68 +42,188 @@
         .done(function(data) {
             var devtitles = new Array();
             var devnums = new Array();
-            var maxdev = 0;
 
+            // Chart type from radio buttons
+            var charttype = $("input[name='graphtype']:checked", graphtype).val();
+            // Chart timeline from selector
+            var charttime = $('select#historyrange option:selected', element).val();
+            // Frequency translation from selector
             var freqtrans = $('select#k_cd_freq_selector option:selected', element).val();
 
-            for (var fk in data['kismet_channeltracker_frequency_map']) {
-                var slot_now =
-                    (data['kismet_channeltracker_frequency_map'][fk]['kismet_channelrec_device_rrd']['kismet_common_rrd_last_time']) % 60;
-                var dev_now = data['kismet_channeltracker_frequency_map'][fk]['kismet_channelrec_device_rrd']['kismet_common_rrd_minute_vec'][slot_now];
+            // historical line chart
+            if (charttype === 'history') {
+                var pointtitles = new Array();
+                var datasets = new Array();
+                var title = "";
 
-                cfk = kismet_ui.GetConvertedChannel(freqtrans, fk);
+                var rrd_type = kismet.RRD_SECOND;
+                var rrd_data = null;
 
-                if (cfk == fk) 
-                    devtitles.push(kismet.HumanReadableFrequency(parseInt(fk)));
-                else
-                    devtitles.push(cfk);
+                if (charttime === 'min') {
+                    title = "Past Minute";
 
-                // devnums.push(dev_now + Math.random() * 100);
-                devnums.push(dev_now);
+                    for (var x = 0; x < 60; x++) {
+                        if (x % 5 == 0) {
+                            pointtitles.push(x);
+                        } else {
+                            pointtitles.push(' ');
+                        }
+                    }
 
-                if (maxdev < dev_now)
-                    maxdev = dev_now;
-            }
+                    rrd_type = kismet.RRD_SECOND;
+                    rrd_data = "kismet_channelrec_device_rrd.kismet_common_rrd_minute_vec";
 
-            if (maxdev == 0)
-                maxdev = 5;
+                } else if (charttime === 'hour') {
+                    title = "Past Hour";
 
-            if (devgraph_chart == null) {
+                    for (var x = 0; x < 60; x++) {
+                        if (x % 5 == 0) {
+                            pointtitles.push(x);
+                        } else {
+                            pointtitles.push(' ');
+                        }
+                    }
+
+                    rrd_type = kismet.RRD_MINUTE;
+                    rrd_data = "kismet_channelrec_device_rrd.kismet_common_rrd_hour_vec";
+
+                } else /* day */ {
+                    title = "Past Day";
+
+                    for (var x = 0; x < 24; x++) {
+                        pointtitles.push(x);
+                    }
+
+                    rrd_type = kismet.RRD_HOUR;
+                    rrd_data = "kismet_channelrec_device_rrd.kismet_common_rrd_day_vec";
+
+                }
+
+                for (var fk in data['kismet_channeltracker_frequency_map']) {
+                    var linedata = 
+                        kismet.RecalcRrdData(
+                            data['kismet_channeltracker_frequency_map'][fk]['kismet_channelrec_device_rrd']['kismet_common_rrd_last_time'], 
+                            last_devicelist_time, 
+                            rrd_type,
+                            kismet.ObjectByString(
+                                data['kismet_channeltracker_frequency_map'][fk], 
+                                rrd_data),
+                            {});
+
+                    // Convert the freq name
+                    var cfk = kismet_ui.GetConvertedChannel(freqtrans, fk);
+
+                    var label = "";
+
+                    if (cfk == fk) 
+                        label = kismet.HumanReadableFrequency(parseInt(fk));
+                    else
+                        label = cfk;
+
+                    // Build the dataset record
+                    var ds = {
+                        label:  label,
+                        fill: false,
+                        lineTension: 0.1,
+                        data: linedata
+                    };
+
+                    // Add it to the dataset
+                    datasets.push(ds);
+                }
+
+                devgraph_canvas.hide();
+                timegraph_canvas.show();
                 coming_soon.hide();
 
-                var device_options = {
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true,
-                                }
-                            }],
+                if (timegraph_chart == null) {
+                    var device_options = {
+                        type: "line",
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true,
+                                    }
+                                }],
+                            },
                         },
-                    },
-                    type: "bar",
-                    data: {
-                        labels: devtitles,
-                        datasets: [
-                            {
-                                label: "Devices per Channel",
-                                backgroundColor: 'rgba(160, 160, 160, 1)',
-                                data: devnums,
-                                borderWidth: 1,
-                            }
-                        ],
-                    }
-                };
+                        data: {
+                            labels: pointtitles,
+                            datasets: datasets,
+                        }
+                    };
 
-                devgraph_chart = new Chart($('.k_cd_dg', element), device_options);
+                    timegraph_chart = new Chart(timegraph_canvas, 
+                        device_options);
+                } else {
+                    timegraph_chart.data.datasets = datasets;
+                    timegraph_chart.data.labels = pointtitles;
 
+                    timegraph_chart.update();
+                }
             } else {
-                devgraph_chart.data.datasets[0].data = devnums;
-                devgraph_chart.data.labels = devtitles;
+                // 'now', but default - if for some reason we didn't get a
+                // value from the selector, this falls through to the bar graph
+                // which is what we probably really want
+                for (var fk in data['kismet_channeltracker_frequency_map']) {
+                    var slot_now =
+                        (data['kismet_channeltracker_frequency_map'][fk]['kismet_channelrec_device_rrd']['kismet_common_rrd_last_time']) % 60;
+                    var dev_now = data['kismet_channeltracker_frequency_map'][fk]['kismet_channelrec_device_rrd']['kismet_common_rrd_minute_vec'][slot_now];
 
-                devgraph_chart.update();
+                    var cfk = kismet_ui.GetConvertedChannel(freqtrans, fk);
+
+                    if (cfk == fk) 
+                        devtitles.push(kismet.HumanReadableFrequency(parseInt(fk)));
+                    else
+                        devtitles.push(cfk);
+
+                    devnums.push(dev_now);
+                }
+
+                devgraph_canvas.show();
+                timegraph_canvas.hide();
+
+                if (devgraph_chart == null) {
+                    coming_soon.hide();
+
+                    var device_options = {
+                        type: "bar",
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true,
+                                    }
+                                }],
+                            },
+                        },
+                        data: {
+                            labels: devtitles,
+                            datasets: [
+                                {
+                                    label: "Devices per Channel",
+                                    backgroundColor: 'rgba(160, 160, 160, 1)',
+                                    data: devnums,
+                                    borderWidth: 1,
+                                }
+                            ],
+                        }
+                    };
+
+                    devgraph_chart = new Chart(devgraph_canvas, 
+                        device_options);
+
+                } else {
+                    devgraph_chart.data.datasets[0].data = devnums;
+                    devgraph_chart.data.labels = devtitles;
+
+                    devgraph_chart.update();
+                }
             }
 
             timerid = setTimeout(channeldisplay_refresh, 5000);
@@ -115,14 +241,88 @@
             options = $.extend(base_options, inopt);
         }
 
+        if (graphtype == null) {
+            graphtype = $('<div>', {
+                "id": "graphtype",
+                "class": "k_cd_type",
+            })
+            .append(
+                $('<label>', {
+                    for: "gt_bar",
+                })
+                .text("Current")
+            )
+            .append($('<input>', {
+                type: "radio",
+                id: "gt_bar",
+                name: "graphtype",
+                value: "now",
+                checked: "checked"
+                })
+            )
+            .append(
+                $('<label>', {
+                    for: "gt_line",
+                })
+                .text("Historical")
+            )
+            .append($('<input>', {
+                type: "radio",
+                name: "graphtype",
+                value: "history"
+            })
+            )
+            .append(
+                $('<select>', {
+                    id: "historyrange",
+                    class: "k_cd_hr_list"
+                })
+                .append(
+                    $('<option>', {
+                        value: "min",
+                    })
+                    .text("Past Minute")
+                )
+                .append(
+                    $('<option>', {
+                        value: "hour",
+                    })
+                    .text("Past Hour")
+                )
+                .append(
+                    $('<option>', {
+                        value: "day",
+                    })
+                    .text("Past Day")
+                )
+                .hide()
+            );
+
+            element.append(graphtype);
+
+            graphtype.on('change', function() {
+                var gt = $("input[name='graphtype']:checked", graphtype). val();
+                console.log("changed graph type " + gt);
+
+                if (gt === 'now') {
+                    $("select#historyrange", graphtype).hide();
+                } else {
+                    $("select#historyrange", graphtype).show();
+                }
+
+                channeldisplay_refresh();
+            });
+
+        }
+
         if (picker == null) {
             picker = $('<div>', {
-                "id": "picker",
-                "class": "k_cd_picker",
+                id: "picker",
+                class: "k_cd_picker",
             });
 
             var sel = $('<select>', {
-                "id": "k_cd_freq_selector",
+                id: "k_cd_freq_selector",
                 class: "k_cd_list",
             });
 
@@ -135,7 +335,7 @@
 
             for (var c in chlist) {
                 var e = $('<option>', {
-                    "value": chlist[c], 
+                    value: chlist[c], 
                 });
                 e.html(chlist[c]);
 
@@ -143,19 +343,29 @@
             }
 
             element.append(picker);
+
+            picker.on('change', function() {
+                channeldisplay_refresh();
+            });
+
         }
 
-        picker.on('change', function() {
-            channeldisplay_refresh();
-        });
-
         devgraph_canvas = $('<canvas>', {
-            "class": "k_cd_dg",
-            "width": "100%",
-            "height": "100%",
+            class: "k_cd_dg",
+            width: "100%",
+            height: "100%",
         });
 
         element.append(devgraph_canvas);
+
+        timegraph_canvas = $('<canvas>', {
+            class: "k_cd_dg",
+            width: "100%",
+            height: "100%"
+        });
+
+        timegraph_canvas.hide();
+        element.append(timegraph_canvas);
 
         // Add a 'coming soon' item
         if (coming_soon == null)  {
