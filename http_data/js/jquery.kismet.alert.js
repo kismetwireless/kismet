d61cf935ade667d3d9fb6cabe329fe9135f69ce6
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2016-12-17 22:51:32 -0500

    Start adding popup panel for alerts

--- http_data/js/jquery.kismet.alert.js
+++ http_data/js/jquery.kismet.alert.js
@@ -23,6 +23,24 @@
 
     var last_time = 0;
 
+    var dialog = null;
+
+    // Close the alert panel if we click outside it
+    var close_dialog_outside = function(e) {
+        if ($(e.target).closest('#alertdialog').length == 0) {
+            if (dialog != null) {
+                dialog.remove();
+                dialog = null;
+            }
+
+            // Remove the handler
+            $('body').off('click', close_dialog_outside);
+
+            // Don't pass the click on
+            e.stopImmediatePropagation();
+        }
+    }
+
     var alert_refresh = function() {
         $.get("/alerts/last-time/" + last_time + "/alerts.json")
         .done(function(data) {
@@ -58,7 +76,7 @@
         alertnum = $('span.number', this);
         if (alertnum.length == 0) {
             alertnum = $('<span>', {
-                class: "number fa fa-fw fa-inverse"
+                class: "number fa fa-stack-1x"
             });
         }
 
@@ -74,9 +92,59 @@
 
         alertholder.append(alertbg);
         alertholder.append(alerticon);
-        //alertholder.append(alertnum);
+        alertholder.append(alertnum);
 
-        element.append(alertholder);
+        var link = $('a.alertbutton', this);
+
+        if (link.length != 0) {
+            link.empty();
+        } else {
+            link = $('<a>', {
+                href: "#",
+                class: "alertbutton"
+            })
+            .on('click', function(e) {
+                var nominal_w = 400;
+                var nominal_h = 200;
+
+                // Basic offset
+                var off_y = (nominal_h / 2) + (element.outerHeight() / 2) + 3;
+                var off_x = (nominal_w / 3);
+
+                // Absolute middle position
+                var mid_e = element.offset().left + (element.outerWidth() / 2);
+
+                // Where the outer border lands
+                var outerborder = mid_e + (nominal_w / 2);
+
+                // If we're outside the window
+                if (outerborder > $(window).width()) {
+                    off_x += ($(window).width() - outerborder);
+                    off_x += 10;
+                }
+
+                dialog = $.jsPanel({
+                    id: "alertdialog",
+                    headerRemove: true,
+                    position: {
+                        of: link,
+                        offsetY: off_y,
+                        offsetX: off_x * - 1
+                    },
+                    contentSize: {
+                        width: nominal_w,
+                        height: nominal_h
+                    },
+                });
+
+                $("body").on("click", close_dialog_outside);
+
+                e.stopImmediatePropagation();
+            });
+        }
+
+        link.append(alertholder);
+        element.append(link);
 
         alert_refresh();
     };
