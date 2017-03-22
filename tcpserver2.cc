1c517e40b028bfeabf1f200946e6405b39840af1
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-03-22 10:31:49 -0400

    Connect tcp server disconnect to ringbuffer

--- tcpserver2.cc
+++ tcpserver2.cc
@@ -339,8 +339,13 @@ bool TcpServerV2::AllowConnection(int in_fd) {
 }
 
 shared_ptr<RingbufferHandler> TcpServerV2::AllocateConnection(int in_fd __attribute__((unused))) {
-    // Exceptionally simple basic allocation
+    // Basic allocation
     shared_ptr<RingbufferHandler> rbh(new RingbufferHandler(ringbuf_size, ringbuf_size));  
+    // Set a callback for errors that kills the connection
+    rbh->SetErrorHandlerCb([this, in_fd](string) {
+            KillConnection(in_fd);
+        });
+
     return rbh;
 }
 
