9c4198dc549814060cb94ff5c27b5e4a6b61129f
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-06-22 14:56:21 -0400

    Initial work on gadgets API

--- http_data/js/kismet.ui.gadgets.js
+++ http_data/js/kismet.ui.gadgets.js
@@ -13,7 +13,93 @@ exports.load_complete = 0;
 
 var gadgets = new Array();
 
+var active_gadgets = new Array();
 
+/* Options for gadgets:
+ * id           unique id of gadget
+ * priority     priority of gadget order
+ * drawfunc     function called for drawing, with arguments:
+ *              container - div for the widget
+ *              data - data for this row
+ */
+
+exports.AddGadget = function(options) {
+    if (!('id' in options)) {
+        console.log("Missing ID in gadget");
+        return -1;
+    }
+
+    if (!('drawfunc' in options)) {
+        console.log("Missing drawfunc in gadget");
+        return -1;
+    }
+
+    if (!('priority' in options)) {
+        options.priority = 0;
+    }
+
+    gadgets.push(options);
+
+    exports.UpdateGadgets();
+};
+
+exports.UpdateGadgets = function() {
+    var newgadgets = new Array();
+
+    gadgets.sort(function(a, b) {
+        if (a.priority < b.priority)
+            return -1;
+        if (a.priority > b.priority)
+            return 1;
+
+        return 0;
+    });
+
+    // Load the saved gadgets
+    var saved_gadgets = kismet.getStorage('kismet.base.gadgets', []);
+
+    saved_gadgets.every(function(e) {
+        // Malformed setting
+        if (!('id' in e))
+            return true;
+
+        if (!('visible' in e))
+            return true;
+
+        // Find the gadget by saved id
+        var g = gadgets.find(function(gi) {
+            return (e.id === gi.id);
+        });
+
+        // Skip gadgets we don't know about anymore
+        if (g === undefined)
+            return true;
+
+        // Add to the list
+        newgadgets.push({
+            id: g.id,
+            gadget: g,
+            visible: e.visisble
+        });
+    });
+
+    // Add any unknown gadgets to the list
+    gadgets.every(function(e) {
+        var g = newgadgets.find(function(ng) {
+            return (e.id === ng.id);
+        });
+
+        if (g === undefined) {
+            newgadgets.push({
+                id: g.id,
+                gadget: g,
+                visisble: true
+            });
+        }
+    });
+
+    active_gadgets = newgadgets;
+}
 
 // We're done loading
 exports.load_complete = 1;
