b5a4080ab3e4362831be12d86a4db336852e728a
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-06-26 09:59:49 -0500

    Add simple bail-out

--- extra/remote_wrapper.sh
+++ extra/remote_wrapper.sh
@@ -1,4 +1,6 @@
-while [ 1 ]; do 
+while [ -f /tmp/kismet_remote/$1 ]; do 
 	/usr/local/bin/kismet_capture_tools/$1 --connect $2 --source $3
 	sleep 5;
 done
+
+print "/tmp/kismet_remote/$1 no longer present, stopping loop"
