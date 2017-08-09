db686ac82d853921924d203d85d5646fddf7ffcb
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-08-08 20:03:31 -0400

    Add device list example

---
+++ rest_examples/basic_kismet_device_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Basic use of the smart_device_list API in the KismetRest python library.
+
+The callback function per_device(dev) is called for each device in the 
+returned list, significantly reducing memory load in high-device-count
+environments.
+
+"""
+
+import sys
+import KismetRest
+import argparse
+import time
+
+def per_device(d):
+    print d['kismet.device.base.macaddr']
+
+uri = "http://localhost:2501"
+
+parser = argparse.ArgumentParser(description='RTL433 to Kismet bridge')
+
+parser.add_argument('--uri', action="store", dest="uri")
+
+results = parser.parse_args()
+
+if results.uri != None:
+    uri = results.uri
+
+kr = KismetRest.KismetConnector(uri)
+
+kr.smart_device_list(callback = per_device)
+
