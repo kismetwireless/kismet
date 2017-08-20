cf97a65b3bbb0d2869e4809e1058a84df082bbbc
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-08-19 23:02:12 -0400

    remove debug

--- gpsgpsd2.cc
+++ gpsgpsd2.cc
@@ -47,7 +47,6 @@ GPSGpsdV2::GPSGpsdV2(GlobalRegistry *in_globalreg, SharedGpsBuilder in_builder) 
     error_reconnect_timer = 
         timetracker->RegisterTimer(SERVER_TIMESLICES_SEC * 10, NULL, 1,
                 [this](int) -> int {
-                    fprintf(stderr, "debug - gpsgpsd2 recon timer connected %d\n", get_device_connected());
                     if (get_device_connected()) 
                         return 1;
 
