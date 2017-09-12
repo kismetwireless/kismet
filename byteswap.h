d0d26e66f73d7ac680f28fbe68e2decb8c413cee
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-09-12 19:10:31 -0400

    Another missing OSX header shim

---
+++ byteswap.h
@@ -0,0 +1,42 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __BYTESWAP_H__
+#define __BYTESWAP_H__
+
+#include "config.h"
+
+#ifndef SYS_DARWIN
+#include <byteswap.h>
+#else
+
+#define bswap_16(value) \
+    ((((value) & 0xff) << 8) | ((value) >> 8))
+
+#define bswap_32(value) \
+    (((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | \
+    (uint32_t)bswap_16((uint16_t)((value) >> 16)))
+
+#define bswap_64(value) \
+    (((uint64_t)bswap_32((uint32_t)((value) & 0xffffffff)) << 32) | \
+    (uint64_t)bswap_32((uint32_t)((value) >> 32)))
+
+#endif
+
+#endif
+
