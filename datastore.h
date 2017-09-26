480338668316b50400db55a6e4020e5958d613b5
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-09-26 17:17:43 -0400

    Minor work on datastore

--- datastore.h
+++ datastore.h
@@ -24,20 +24,35 @@
 /* Generic data store based on sqlite3 */
 
 #include <memory>
+#include <mutex>
+#include <string>
 
 #include <stdio.h>
 #include <sqlite3.h>
 
 #include "globalregistry.h"
 
+/* Kismet Datastores
+ *
+ * Each data store is an independent database file.
+ *
+ * Each database contains one core table which contains information about
+ * the primary module manipulating state, the database version, and the kismet
+ * version which last accessed the file.
+ *
+ * Database versions are monotonically incrementing numbers; a module is
+ * expected to detect when the codebase is newer than the database file and
+ * perform any upgrades necessary.
+ *
+ * Providers of datastore objects must subclass the datastore and implement an
+ * API to abstract the internal SQL commands; callers should not be expected to
+ * implement SQL directly.
+ *
+ */
+
 class Datastore : public LifetimeGlobal {
-public:
-    static shared_ptr<Datastore> create_datastore(GlobalRegistry *in_globalreg) {
-        shared_ptr<Datastore> mon(new Datastore(in_globalreg));
-        in_globalreg->RegisterLifetimeGLobal(mon);
-        in_globalreg->InsertGlobal("DATASTORE", mon);
-        return mon;
-    }
+    // Subclasses must implement a public builder, typically one which returns a
+    // shared pointer instance
 
 private:
     Datastore(GlobalRegistry *in_globalreg);
@@ -45,6 +60,24 @@ private:
 public:
     virtual ~Datastore();
 
+    virtual unsigned int get_db_version();
+
+    // Initialize a new database
+    virtual unsigned int initialize_db() = 0;
+
+    // Upgrade a database which doesn't match our version
+    virtual unsigned int upgrade_db(unsigned int version) = 0;
+
+private:
+    GlobalRegistry *globalreg;
+
+    // Module name and target version, filled in by subclasses during initialization
+    std::string ds_module_name;
+    unsigned int ds_target_version;
+
+    std::recursive_timed_mutex ds_mutex;
+
+    sqlite3 *db;
 };
 
 
