2afeca36e5487988c060883c8d911a7ba2dc738b
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-11-07 09:27:02 -0500

    Make fstream compile on osx

--- strict_fstream.hpp
+++ strict_fstream.hpp
@@ -39,7 +39,7 @@ static std::string strerror()
     {
         buff = "Unknown error";
     }
-#elif (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+#elif (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE || __APPLE__
 // XSI-compliant strerror_r()
     if (strerror_r(errno, &buff[0], buff.size()) != 0)
     {
