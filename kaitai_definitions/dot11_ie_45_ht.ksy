86e4bfcae7ac5b5ea3f490bf1d7f4b41396c1bd9
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-12-07 10:08:52 -0500

    Add dot11 IE45 kaitai parser

---
+++ kaitai_definitions/dot11_ie_45_ht.ksy
@@ -0,0 +1,81 @@
+meta:
+  id: dot11_ie_45_ht
+  file-extension: dot11_ie_45_ht
+
+seq:
+  - id: ht_capabilities
+    type: u2le
+  - id: ampdu
+    type: u1
+  - id: mcs
+    type: rx_mcs
+  - id: ht_extended_caps
+    type: u2le
+  - id: txbf_caps
+    type: u4le
+  - id: asel_caps
+    type: u1
+    
+types:
+  rx_mcs:
+    seq:
+      - id: rx_mcs_b0
+        type: u1
+      - id: rx_mcs_b1
+        type: u1
+      - id: rx_mcs_b2
+        type: u1
+      - id: rx_mcs_b3
+        type: u1
+      - id: rx_mcs_b4
+        type: u1
+      - id: rx_mcs_b5
+        type: u1
+      - id: rx_mcs_b6
+        type: u1
+      - id: rx_mcs_b7
+        type: u1
+      - id: rx_mcs_b8
+        type: u1
+      - id: rx_mcs_b9
+        type: u1
+      - id: supported_data_rate
+        type: u2le
+      - id: txflags
+        type: u1
+    instances:
+      ht_num_streams:
+        value: "(rx_mcs_b0 != 0 ? 1 : 0) + (rx_mcs_b1 != 0 ? 1 : 0) + (rx_mcs_b2 != 0 ? 1 : 0) + (rx_mcs_b3 != 0 ? 1 : 0)"
+
+        
+instances:
+  ht_cap_ldpc:
+    value: "ht_capabilities & 0x01"
+  ht_cap_40mhz_intolerant:
+    value: "ht_capabilities & 0x02"
+  ht_cap_sm_powersave:
+    value: "ht_capabilities & 0x0C"
+  ht_cap_greenfield:
+    value: "ht_capabilities & 0x10"
+  ht_cap_20mhz_shortgi:
+    value: "ht_capabilities & 0x20"
+  ht_cap_40mhz_shortgi:
+    value: "ht_capabilities & 0x40"
+  ht_cap_tx_stbc:
+    value: "ht_capabilities & 0x80"
+  ht_cap_rx_stbc:
+    value: "ht_capabilities & 0x300"
+  ht_cap_delayed_block_ack:
+    value: "ht_capabilities & 0x400"
+  ht_cap_max_amsdu_len:
+    value: "ht_capabilities & 0x800"
+  ht_cap_dss_40mhz:
+    value: "ht_capabilities & 0x1000"
+  ht_cap_psmp_intolerant:
+    value: "ht_capabilities & 0x2000"
+  ht_cap_40mhz_intolerant:
+    value: "ht_capabilities & 0x4000"
+  ht_cap_lsig_txop:
+    value: "ht_capabilities & 0x8000"
+
+      
---
+++ kaitai_parsers/dot11_ie_45_ht.cc
@@ -0,0 +1,167 @@
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+#include "dot11_ie_45_ht.h"
+
+#include <iostream>
+#include <fstream>
+
+dot11_ie_45_ht_t::dot11_ie_45_ht_t(kaitai::kstream *p_io, kaitai::kstruct *p_parent, dot11_ie_45_ht_t *p_root) : kaitai::kstruct(p_io) {
+    m__parent = p_parent;
+    m__root = this;
+    f_ht_cap_dss_40mhz = false;
+    f_ht_cap_rx_stbc = false;
+    f_ht_cap_max_amsdu_len = false;
+    f_ht_cap_40mhz_intolerant = false;
+    f_ht_cap_sm_powersave = false;
+    f_ht_cap_tx_stbc = false;
+    f_ht_cap_greenfield = false;
+    f_ht_cap_ldpc = false;
+    f_ht_cap_20mhz_shortgi = false;
+    f_ht_cap_delayed_block_ack = false;
+    f_ht_cap_lsig_txop = false;
+    f_ht_cap_40mhz_shortgi = false;
+    f_ht_cap_psmp_intolerant = false;
+    m_ht_capabilities = m__io->read_u2le();
+    m_ampdu = m__io->read_u1();
+    m_mcs = new rx_mcs_t(m__io, this, m__root);
+    m_ht_extended_caps = m__io->read_u2le();
+    m_txbf_caps = m__io->read_u4le();
+    m_asel_caps = m__io->read_u1();
+}
+
+dot11_ie_45_ht_t::~dot11_ie_45_ht_t() {
+    delete m_mcs;
+}
+
+dot11_ie_45_ht_t::rx_mcs_t::rx_mcs_t(kaitai::kstream *p_io, dot11_ie_45_ht_t *p_parent, dot11_ie_45_ht_t *p_root) : kaitai::kstruct(p_io) {
+    m__parent = p_parent;
+    m__root = p_root;
+    f_ht_num_streams = false;
+    m_rx_mcs_b0 = m__io->read_u1();
+    m_rx_mcs_b1 = m__io->read_u1();
+    m_rx_mcs_b2 = m__io->read_u1();
+    m_rx_mcs_b3 = m__io->read_u1();
+    m_rx_mcs_b4 = m__io->read_u1();
+    m_rx_mcs_b5 = m__io->read_u1();
+    m_rx_mcs_b6 = m__io->read_u1();
+    m_rx_mcs_b7 = m__io->read_u1();
+    m_rx_mcs_b8 = m__io->read_u1();
+    m_rx_mcs_b9 = m__io->read_u1();
+    m_supported_data_rate = m__io->read_u2le();
+    m_txflags = m__io->read_u1();
+}
+
+dot11_ie_45_ht_t::rx_mcs_t::~rx_mcs_t() {
+}
+
+int32_t dot11_ie_45_ht_t::rx_mcs_t::ht_num_streams() {
+    if (f_ht_num_streams)
+        return m_ht_num_streams;
+    m_ht_num_streams = (((((rx_mcs_b0() != 0) ? (1) : (0)) + ((rx_mcs_b1() != 0) ? (1) : (0))) + ((rx_mcs_b2() != 0) ? (1) : (0))) + ((rx_mcs_b3() != 0) ? (1) : (0)));
+    f_ht_num_streams = true;
+    return m_ht_num_streams;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_dss_40mhz() {
+    if (f_ht_cap_dss_40mhz)
+        return m_ht_cap_dss_40mhz;
+    m_ht_cap_dss_40mhz = (ht_capabilities() & 4096);
+    f_ht_cap_dss_40mhz = true;
+    return m_ht_cap_dss_40mhz;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_rx_stbc() {
+    if (f_ht_cap_rx_stbc)
+        return m_ht_cap_rx_stbc;
+    m_ht_cap_rx_stbc = (ht_capabilities() & 768);
+    f_ht_cap_rx_stbc = true;
+    return m_ht_cap_rx_stbc;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_max_amsdu_len() {
+    if (f_ht_cap_max_amsdu_len)
+        return m_ht_cap_max_amsdu_len;
+    m_ht_cap_max_amsdu_len = (ht_capabilities() & 2048);
+    f_ht_cap_max_amsdu_len = true;
+    return m_ht_cap_max_amsdu_len;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_40mhz_intolerant() {
+    if (f_ht_cap_40mhz_intolerant)
+        return m_ht_cap_40mhz_intolerant;
+    m_ht_cap_40mhz_intolerant = (ht_capabilities() & 16384);
+    f_ht_cap_40mhz_intolerant = true;
+    return m_ht_cap_40mhz_intolerant;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_sm_powersave() {
+    if (f_ht_cap_sm_powersave)
+        return m_ht_cap_sm_powersave;
+    m_ht_cap_sm_powersave = (ht_capabilities() & 12);
+    f_ht_cap_sm_powersave = true;
+    return m_ht_cap_sm_powersave;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_tx_stbc() {
+    if (f_ht_cap_tx_stbc)
+        return m_ht_cap_tx_stbc;
+    m_ht_cap_tx_stbc = (ht_capabilities() & 128);
+    f_ht_cap_tx_stbc = true;
+    return m_ht_cap_tx_stbc;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_greenfield() {
+    if (f_ht_cap_greenfield)
+        return m_ht_cap_greenfield;
+    m_ht_cap_greenfield = (ht_capabilities() & 16);
+    f_ht_cap_greenfield = true;
+    return m_ht_cap_greenfield;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_ldpc() {
+    if (f_ht_cap_ldpc)
+        return m_ht_cap_ldpc;
+    m_ht_cap_ldpc = (ht_capabilities() & 1);
+    f_ht_cap_ldpc = true;
+    return m_ht_cap_ldpc;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_20mhz_shortgi() {
+    if (f_ht_cap_20mhz_shortgi)
+        return m_ht_cap_20mhz_shortgi;
+    m_ht_cap_20mhz_shortgi = (ht_capabilities() & 32);
+    f_ht_cap_20mhz_shortgi = true;
+    return m_ht_cap_20mhz_shortgi;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_delayed_block_ack() {
+    if (f_ht_cap_delayed_block_ack)
+        return m_ht_cap_delayed_block_ack;
+    m_ht_cap_delayed_block_ack = (ht_capabilities() & 1024);
+    f_ht_cap_delayed_block_ack = true;
+    return m_ht_cap_delayed_block_ack;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_lsig_txop() {
+    if (f_ht_cap_lsig_txop)
+        return m_ht_cap_lsig_txop;
+    m_ht_cap_lsig_txop = (ht_capabilities() & 32768);
+    f_ht_cap_lsig_txop = true;
+    return m_ht_cap_lsig_txop;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_40mhz_shortgi() {
+    if (f_ht_cap_40mhz_shortgi)
+        return m_ht_cap_40mhz_shortgi;
+    m_ht_cap_40mhz_shortgi = (ht_capabilities() & 64);
+    f_ht_cap_40mhz_shortgi = true;
+    return m_ht_cap_40mhz_shortgi;
+}
+
+int32_t dot11_ie_45_ht_t::ht_cap_psmp_intolerant() {
+    if (f_ht_cap_psmp_intolerant)
+        return m_ht_cap_psmp_intolerant;
+    m_ht_cap_psmp_intolerant = (ht_capabilities() & 8192);
+    f_ht_cap_psmp_intolerant = true;
+    return m_ht_cap_psmp_intolerant;
+}
