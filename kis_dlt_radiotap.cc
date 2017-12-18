0f51e27f7ff2f1003cfa0020e064d356162ee2f1
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-12-18 14:49:13 -0500

    Finish commenting out errors

--- kis_dlt_radiotap.cc
+++ kis_dlt_radiotap.cc
@@ -193,7 +193,7 @@ int Kis_DLT_Radiotap::HandlePacket(kis_packet *in_pack) {
 
     if (linkchunk->length < sizeof(*hdr)) {
 		// snprintf(errstr, STATUS_MAX, "pcap radiotap converter got corrupted " "Radiotap header length");
-		globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
+		// globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
         return 0;
     }
 
@@ -201,7 +201,7 @@ int Kis_DLT_Radiotap::HandlePacket(kis_packet *in_pack) {
     hdr = (struct ieee80211_radiotap_header *) linkchunk->data;
     if (linkchunk->length < EXTRACT_LE_16BITS(&hdr->it_len)) {
 		// snprintf(errstr, STATUS_MAX, "pcap radiotap converter got corrupted " "Radiotap header length");
-		globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
+		// globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
         return 0;
     }
 
@@ -214,7 +214,7 @@ int Kis_DLT_Radiotap::HandlePacket(kis_packet *in_pack) {
     /* are there more bitmap extensions than bytes in header? */
     if ((EXTRACT_LE_32BITS(last_presentp) & BIT(IEEE80211_RADIOTAP_EXT)) != 0) {
 		// snprintf(errstr, STATUS_MAX, "pcap radiotap converter got corrupted " "Radiotap bitmap length");
-		globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
+		// globalreg->messagebus->InjectMessage(errstr, MSGFLAG_ERROR);
         return 0;
     }
 
