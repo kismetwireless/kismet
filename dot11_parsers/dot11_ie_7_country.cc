10b5e1283a0df8d39cde96fb2347dd48c5ae843c
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-02-01 16:57:26 -0500

    Port ie7 country code (which clued us into the kaitai problem in the         first place)

---
+++ dot11_parsers/dot11_ie_7_country.cc
@@ -0,0 +1,47 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "dot11_ie_7_country.h"
+
+void dot11_ie_7_country::parse(std::shared_ptr<kaitai::kstream> p_io) {
+    m_country_code = p_io->read_bytes(2);
+    m_environment = p_io->read_u1();
+    m_country_list.reset(new shared_dot11d_country_triplet_vector());
+    while (!p_io->is_eof()) {
+        // Do our best to read all the channel codings; if we allow broken
+        // country tags, read as far as we can and then stop, otherwise
+        // pass the error upstream
+        try {
+            std::shared_ptr<dot11d_country_triplet> c(new dot11d_country_triplet());
+            c->parse(p_io);
+            m_country_list->push_back(c);
+        } catch (std::exception& e) {
+            if (i_allow_fragments)
+                break;
+            else
+                throw(e);
+        }
+    }
+}
+
+void dot11_ie_7_country::dot11d_country_triplet::parse(std::shared_ptr<kaitai::kstream> p_io) {
+    m_first_channel = p_io->read_u1();
+    m_num_channels = p_io->read_u1();
+    m_max_power = p_io->read_u1();
+}
+
---
+++ dot11_parsers/dot11_ie_7_country.h
@@ -0,0 +1,106 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __DOT11_IE_7_COUNTRY_H__
+#define __DOT11_IE_7_COUNTRY_H__
+
+/* dot11 ie 7 country
+ *
+ * 80211d is deprecated.
+ *
+ * Some APs seem to return bogus country strings which don't include a valid
+ * set of triplets.
+ *
+ */
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <kaitai/kaitaistream.h>
+
+class dot11_ie_7_country {
+public:
+    class dot11d_country_triplet;
+    typedef std::vector<std::shared_ptr<dot11d_country_triplet> > shared_dot11d_country_triplet_vector;
+
+    dot11_ie_7_country() {
+        i_allow_fragments = false;
+    }
+    ~dot11_ie_7_country() { }
+
+    // Some APs send bogon lists w/ a non-triplet set of channels at the
+    // end when sending long, weird ranges of single-channel ranges.
+    // If we allow fragments, we silently fail the last channel being
+    // invalid
+    void set_allow_fragments(bool in_f) {
+        i_allow_fragments = in_f;
+    }
+
+    void parse(std::shared_ptr<kaitai::kstream> p_io);
+
+    std::string country_code() {
+        return m_country_code;
+    }
+
+    uint8_t environment() {
+        return m_environment;
+    }
+
+    std::shared_ptr<shared_dot11d_country_triplet_vector> country_list() {
+        return m_country_list;
+    }
+
+protected:
+    std::string m_country_code;
+    uint8_t m_environment;
+    std::shared_ptr<shared_dot11d_country_triplet_vector> m_country_list;
+
+    bool i_allow_fragments;
+
+public:
+    class dot11d_country_triplet {
+    public:
+        dot11d_country_triplet() {}
+        ~dot11d_country_triplet() {}
+
+        void parse(std::shared_ptr<kaitai::kstream> p_io);
+
+        uint8_t first_channel() {
+            return m_first_channel;
+        }
+
+        uint8_t num_channels() {
+            return m_num_channels;
+        }
+
+        uint8_t max_power() {
+            return m_max_power;
+        }
+
+    protected:
+        uint8_t m_first_channel;
+        uint8_t m_num_channels;
+        uint8_t m_max_power;
+    };
+
+
+};
+
+
+#endif
+
