592202155e36384c26668e3ac4f2d5cc1de9ad6c
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-01-31 20:28:33 -0500

    Convert IE48

---
+++ dot11_parsers/dot11_ie_48_rsn.cc
@@ -0,0 +1,51 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "dot11_ie_48_rsn.h"
+
+void dot11_ie_48_rsn::parse(kaitai::kstream *p_io) {
+    m_rsn_version = p_io->read_u2le();
+    m_group_cipher.reset(new dot11_ie_48_rsn::dot11_ie_48_rsn_rsn_cipher());
+    m_group_cipher->parse(p_io);
+    m_pairwise_count = p_io->read_u2le();
+    m_pairwise_ciphers.reset(new shared_rsn_cipher_vector());
+    for (unsigned int i = 0; i < pairwise_count(); i++) {
+        std::shared_ptr<dot11_ie_48_rsn_rsn_cipher> c(new dot11_ie_48_rsn_rsn_cipher());
+        c->parse(p_io);
+        m_pairwise_ciphers->push_back(c);
+    }
+    m_akm_count = p_io->read_u2le();
+    m_akm_ciphers.reset(new shared_rsn_management_vector());
+    for (unsigned int i = 0; i < akm_count(); i++) {
+        std::shared_ptr<dot11_ie_48_rsn_rsn_management> a(new dot11_ie_48_rsn_rsn_management());
+        a->parse(p_io);
+        m_akm_ciphers->push_back(a);
+    }
+
+}
+
+void dot11_ie_48_rsn::dot11_ie_48_rsn_rsn_cipher::parse(kaitai::kstream *p_io) {
+    m_cipher_suite_oui = p_io->read_bytes(3);
+    m_cipher_type = p_io->read_u1();
+}
+
+void dot11_ie_48_rsn::dot11_ie_48_rsn_rsn_management::parse(kaitai::kstream *p_io) {
+    m_management_suite_oui = p_io->read_bytes(3);
+    m_management_type = p_io->read_u1();
+}
+
---
+++ dot11_parsers/dot11_ie_48_rsn.h
@@ -0,0 +1,148 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __DOT11_IE_H__
+#define __DOT11_IE_H__
+
+/* Parse dot11 IE 48 - RSN
+ *
+ * RSN (Robust Security Network) defines 802.11i WPA/WPA2 encryption
+ *
+ */
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <kaitai/kaitaistream.h>
+
+class dot11_ie_48_rsn {
+public:
+    class dot11_ie_48_rsn_rsn_cipher;
+    class dot11_ie_48_rsn_rsn_management;
+
+    typedef std::vector<std::shared_ptr<dot11_ie_48_rsn_rsn_cipher> > shared_rsn_cipher_vector;
+    typedef std::vector<std::shared_ptr<dot11_ie_48_rsn_rsn_management> > shared_rsn_management_vector;
+
+    dot11_ie_48_rsn() { }
+    ~dot11_ie_48_rsn() { }
+
+    void parse(kaitai::kstream *p_io);
+
+    uint16_t rsn_version() {
+        return m_rsn_version;
+    }
+
+    std::shared_ptr<dot11_ie_48_rsn_rsn_cipher> group_cipher() {
+        return m_group_cipher;
+    }
+
+    uint16_t pairwise_count() {
+        return m_pairwise_count;
+    }
+
+    std::shared_ptr<shared_rsn_cipher_vector> pairwise_ciphers() {
+        return m_pairwise_ciphers;
+    }
+
+    uint16_t akm_count() {
+        return m_akm_count;
+    }
+
+    std::shared_ptr<shared_rsn_management_vector> akm_ciphers() {
+        return m_akm_ciphers;
+    }
+
+protected:
+    uint16_t m_rsn_version;
+    std::shared_ptr<dot11_ie_48_rsn_rsn_cipher> m_group_cipher;
+    uint16_t m_pairwise_count;
+    std::shared_ptr<shared_rsn_cipher_vector> m_pairwise_ciphers;
+    uint16_t m_akm_count;
+    std::shared_ptr<shared_rsn_management_vector> m_akm_ciphers;
+
+public:
+    class dot11_ie_48_rsn_rsn_cipher {
+    public:
+        enum rsn_cipher_type {
+            rsn_cipher_none = 0,
+            rsn_wep_40 = 1,
+            rsn_tkip = 2,
+            rsn_aes_ocb = 3,
+            rsn_aes_ccm = 4,
+            rsn_wep_104 = 5,
+            rsn_bip = 6,
+            rsn_no_group = 7,
+            rsn_gcmp = 8
+        };
+
+        dot11_ie_48_rsn_rsn_cipher() { }
+
+        ~dot11_ie_48_rsn_rsn_cipher() { }
+
+        void parse(kaitai::kstream *p_io);
+
+        std::string cipher_suite_oui() {
+            return m_cipher_suite_oui;
+        }
+
+        rsn_cipher_type cipher_type() {
+            return (rsn_cipher_type) m_cipher_type;
+        }
+
+
+    protected:
+        std::string m_cipher_suite_oui;
+        uint8_t m_cipher_type;
+    };
+
+    class dot11_ie_48_rsn_rsn_management {
+    public:
+        enum rsn_management {
+            mgmt_none = 0,
+            mgmt_wpa = 1,
+            mgmt_psk = 2,
+            mgmt_ft_dot1x = 3,
+            mgmt_ft_psk = 4,
+            mgmt_wpa_sha256 = 5,
+            mgmt_psk_sha256 = 6,
+            mgmt_tdls_tpk = 7
+        };
+
+        dot11_ie_48_rsn_rsn_management() { }
+        ~dot11_ie_48_rsn_rsn_management() { }
+
+        void parse(kaitai::kstream *p_io);
+
+        std::string management_suite_oui() {
+            return m_management_suite_oui;
+        }
+
+        rsn_management management_type() {
+            return (rsn_management) m_management_type;
+        }
+
+    protected:
+        std::string m_management_suite_oui;
+        uint8_t m_management_type;
+    };
+
+};
+
+
+#endif
+
