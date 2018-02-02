ebc54507a6dda02a9f5e7090c3bcae63bc1094ad
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-02-01 23:34:19 -0500

    Port dot11 action frame

---
+++ dot11_parsers/dot11_action.cc
@@ -0,0 +1,37 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "dot11_action.h"
+
+void dot11_action::parse(std::shared_ptr<kaitai::kstream> p_io) {
+    m_category_code = p_io->read_u1();
+    m_action_data = p_io->read_bytes_full();
+    m_action_data_stream.reset(new kaitai::kstream(m_action_data));
+    if (category_code() == category_code_radio_measurement) {
+        std::shared_ptr<action_rmm> r(new action_rmm());
+        r->parse(m_action_data_stream);
+        m_action_frame = r;
+    }
+}
+
+void dot11_action::action_rmm::parse(std::shared_ptr<kaitai::kstream> p_io) {
+    m_rmm_action_code = p_io->read_u1();
+    m_dialog_token = p_io->read_u1();
+    m_tags_data = p_io->read_bytes_full();
+    m_tags_data_stream.reset(new kaitai::kstream(m_tags_data));
+}
---
+++ dot11_parsers/dot11_action.h
@@ -0,0 +1,153 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __DOT11_ACTION_H__
+#define __DOT11_ACTION_H__
+
+/* dot11 action frame
+ *
+ * dot11 action frames look a lot like management frames, but with a 
+ * custom frame control header.
+ *
+ * Some of the IE tag parsing overlaps existing IE tag parsers
+ *
+ */
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <kaitai/kaitaistream.h>
+
+class dot11_action {
+public:
+    class action_frame_common;
+    class action_rmm;
+
+    enum category_code_type_e {
+        category_code_spectrum_management = 0,
+        category_code_qos = 1,
+        category_code_dls = 2,
+        category_code_block_ack = 3,
+        category_code_public = 4,
+        category_code_radio_measurement = 5,
+        category_code_fastbss = 6,
+        category_code_ht = 7,
+        category_code_sa_query = 8,
+        category_code_public_protected = 9,
+        category_code_wnm = 10,
+        category_code_unprotected_wnm = 11,
+        category_code_tlds = 12,
+        category_code_mesh = 13,
+        category_code_multihop = 14,
+        category_code_self_protected = 15,
+        category_code_dmg = 16,
+        category_code_mgmt_notification = 17,
+        category_code_fast_session_transfer = 18,
+        category_code_robust_av_stream = 19,
+        category_code_ubprotected_dmg = 20,
+        category_code_vht = 21,
+        category_code_vendor_specific_protected = 126,
+        category_code_vendor_specific = 127
+    };
+
+    dot11_action() { }
+    ~dot11_action() { }
+
+    void parse(std::shared_ptr<kaitai::kstream> p_io);
+
+    category_code_type_e category_code() {
+        return (category_code_type_e) m_category_code;
+    }
+
+    std::string action_rmm_data() {
+        return m_action_data;
+    }
+
+    std::shared_ptr<kaitai::kstream> action_rmm_data_stream() {
+        return m_action_data_stream;
+    }
+
+    std::shared_ptr<action_frame_common> action_frame() {
+        return m_action_frame;
+    }
+
+    std::shared_ptr<action_rmm> action_frame_rmm() {
+        if (category_code() == category_code_radio_measurement) 
+            return std::static_pointer_cast<action_rmm>(action_frame());
+        return NULL;
+    }
+
+protected:
+    uint8_t m_category_code;
+    std::string m_action_data;
+    std::shared_ptr<kaitai::kstream> m_action_data_stream;
+    std::shared_ptr<action_frame_common> m_action_frame;
+
+public:
+    class action_frame_common {
+    public:
+        action_frame_common() {}
+        virtual ~action_frame_common() {}
+    };
+
+    class action_rmm : public action_frame_common {
+    public:
+        enum rmm_action_type_e {
+            rmm_action_measurement_req = 0,
+            rmm_action_measurement_report = 1,
+            rmm_action_link_measurement_req = 2,
+            rmm_action_link_measurement_report = 3,
+            rmm_action_neighbor_req = 4,
+            rmm_action_neighbor_report = 5
+        };
+
+        action_rmm() {}
+        virtual ~action_rmm() {}
+
+        void parse(std::shared_ptr<kaitai::kstream> p_io);
+
+        rmm_action_type_e rmm_action_code() {
+            return (rmm_action_type_e) m_rmm_action_code;
+        }
+
+        uint8_t dialog_token() {
+            return m_dialog_token;
+        }
+
+        std::string tags_data() {
+            return m_tags_data;
+        }
+
+        std::shared_ptr<kaitai::kstream> tags_data_stream() {
+            return m_tags_data_stream;
+        }
+
+    protected:
+        uint8_t m_rmm_action_code;
+        uint8_t m_dialog_token;
+        std::string m_tags_data;
+        std::shared_ptr<kaitai::kstream> m_tags_data_stream;
+
+    };
+
+
+};
+
+
+#endif
+
