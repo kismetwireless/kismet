b75115d0f96fc80d40d1b27d736da165275a19d4
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-03-04 14:38:26 -0500

    Default to metric if no preference set

--- http_data/js/kismet.ui.js
+++ http_data/js/kismet.ui.js
@@ -619,7 +619,8 @@ exports.DegToDir = function(deg) {
 
 // Use our settings to make some conversion functions for distance and temperature
 exports.renderDistance = function(k, precision = 5) {
-    if (kismet.getStorage('kismet.base.unit.distance') === 'metric') {
+    if (kismet.getStorage('kismet.base.unit.distance') === 'metric' ||
+            kismet.getStorage('kismet.base.unit.distance') === '') {
         if (k < 1) {
             return (k * 1000).toFixed(precision) + ' m';
         }
@@ -637,7 +638,8 @@ exports.renderDistance = function(k, precision = 5) {
 
 // Use our settings to make some conversion functions for distance and temperature
 exports.renderHeightDistance = function(m, precision = 5) {
-    if (kismet.getStorage('kismet.base.unit.distance') === 'metric') {
+    if (kismet.getStorage('kismet.base.unit.distance') === 'metric' ||
+            kismet.getStorage('kismet.base.unit.distance') === '') {
         if (m < 1000) {
             return m.toFixed(precision) + ' m';
         }
@@ -654,7 +656,8 @@ exports.renderHeightDistance = function(m, precision = 5) {
 }
 
 exports.renderSpeed = function(kph, precision = 5) {
-    if (kismet.getStorage('kismet.base.unit.speed') === 'metric') {
+    if (kismet.getStorage('kismet.base.unit.speed') === 'metric' ||
+            kismet.getStorage('kismet.base.unit.speed') === '') {
         return kph.toFixed(precision) + ' KPH';
     } else {
         return (kph * 0.621371).toFixed(precision) + ' MPH';
@@ -662,7 +665,8 @@ exports.renderSpeed = function(kph, precision = 5) {
 }
 
 exports.renderTemperature = function(c, precision = 5) {
-    if (kismet.getStorage('kismet.base.unit.temp') === 'celcius') {
+    if (kismet.getStorage('kismet.base.unit.temp') === 'celcius' ||
+            kismet.getStorage('kismet.base.unit.temp') === '') {
         return c.toFixed(precision) + '&deg; C';
     } else {
         return (c * (9/5) + 32).toFixed(precision) + '&deg; F';
