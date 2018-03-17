90b2ddb186f736e538cd2af03777644d2f19989f
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-03-17 17:52:23 -0400

    Start implementing POC of http request api in python library

--- python_tools/KismetExternal/KismetExternal/__init__.py
+++ python_tools/KismetExternal/KismetExternal/__init__.py
@@ -33,9 +33,6 @@ class KismetExternalInterface:
         fl = fcntl.fcntl(outfd, fcntl.F_GETFL)
         fcntl.fcntl(outfd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
-        self.input = os.fdopen(infd, 'rb')
-        self.output = os.fdopen(outfd, 'wb')
-
         self.wbuffer = ""
         self.rbuffer = ""
 
@@ -45,10 +42,15 @@ class KismetExternalInterface:
 
         self.last_pong = 0
 
+        self.running = False
+
         self.handlers = {}
 
+        self.add_handler("HTTPREQUEST", self.handle_http_request)
         self.add_handler("PING", self.handle_ping)
         self.add_handler("PONG", self.handle_pong)
+
+        self.uri_handlers = {}
 
     def __adler32(self, data):
         if len(data) < 4:
@@ -57,55 +59,63 @@ class KismetExternalInterface:
         s1 = 0
         s2 = 0
 
+        last_i = 0
+
         for i in range(0, len(data) - 4, 4):
             s2 += 4 * (s1 + ord(data[i])) + 3 * ord(data[i + 1]) + 2 * ord(data[i + 2]) + ord(data[i + 3])
             s1 += ord(data[i + 0]) + ord(data[i + 1]) + ord(data[i + 2]) + ord(data[i + 3])
+            last_i = i + 4
 
-        extra = len(data) % 4
-        for i in range(len(data) - extra, len(data)):
+        for i in range(last_i, len(data)):
             s1 += ord(data[i])
             s2 += s1
 
-        return (s1 & 0xFFFF) + (s2 << 16)
+        return ((s1 & 0xFFFF) + (s2 << 16)) & 0xFFFFFFFF
 
     def __io_loop(self):
-        while not self.kill_ioloop:
-            if not self.last_pong == 0 and time.time() - self.last_pong > 5:
-                raise RuntimeError("No PONG from remote system in 5 seconds")
+        try:
+            while not self.kill_ioloop:
+                if not self.last_pong == 0 and time.time() - self.last_pong > 5:
+                    raise RuntimeError("No PONG from remote system in 5 seconds")
 
-            inputs = [ self.input ]
-            outputs = []
+                inputs = [ self.infd ]
+                outputs = []
 
-            self.bufferlock.acquire()
-            try:
-                if len(self.wbuffer):
-                    outputs = [ self.output ]
-            finally:
-                self.bufferlock.release()
-
-            (readable, writable, exceptional) = select.select(inputs, outputs, inputs, 1)
-
-            if self.output in outputs:
                 self.bufferlock.acquire()
                 try:
-                    written = os.write(self.outfd, self.wbuffer)
-                    self.wbuffer = self.wbuffer[written:]
-                except OSError as e:
-                    if not e.errno == errno.EAGAIN:
-                        raise BufferError("Output buffer error: {}".format(e))
+                    if len(self.wbuffer):
+                        outputs = [ self.outfd ]
                 finally:
                     self.bufferlock.release()
 
-            if self.input in inputs:
-                self.bufferlock.acquire()
-                try:
-                    self.rbuffer = self.rbuffer + os.read(self.infd, 4096)
-                    self.__recv_packet()
-                except OSError as e:
-                    if not e.errno == errno.EAGAIN:
-                        raise BufferError("Input buffer error: {}".format(e))
-                finally:
-                    self.bufferlock.release()
+                (readable, writable, exceptional) = select.select(inputs, outputs, inputs, 1)
+
+                if self.outfd in exceptional or self.infd in exceptional:
+                    raise BufferError("Buffer error:  Socket closed")
+
+                if self.outfd in outputs:
+                    self.bufferlock.acquire()
+                    try:
+                        written = os.write(self.outfd, self.wbuffer)
+                        self.wbuffer = self.wbuffer[written:]
+                    except OSError as e:
+                        if not e.errno == errno.EAGAIN:
+                            raise BufferError("Output buffer error: {}".format(e))
+                    finally:
+                        self.bufferlock.release()
+
+                if self.infd in inputs:
+                    self.bufferlock.acquire()
+                    try:
+                        self.rbuffer = self.rbuffer + os.read(self.infd, 4096)
+                        self.__recv_packet()
+                    except OSError as e:
+                        if not e.errno == errno.EAGAIN:
+                            raise BufferError("Input buffer error: {}".format(e))
+                    finally:
+                        self.bufferlock.release()
+        finally:
+            self.running = False
 
     def __recv_packet(self):
         if len(self.rbuffer) < 12:
@@ -124,7 +134,8 @@ class KismetExternalInterface:
         calc_csum = self.__adler32(content)
 
         if not calc_csum == checksum:
-            raise BufferError("Invalid checksum in packet header")
+            print content.encode('hex')
+            raise BufferError("Invalid checksum in packet header {} vs {}".format(calc_csum, checksum))
 
         cmd = kismet_pb2.Command()
         cmd.ParseFromString(content)
@@ -137,11 +148,29 @@ class KismetExternalInterface:
         self.rbuffer = self.rbuffer[12 + sz:]
 
     def start(self):
+        self.running = True
         self.iothread = threading.Thread(target=self.__io_loop)
         self.iothread.start()
 
     def add_handler(self, command, handler):
         self.handlers[command] = handler
+
+    def add_uri_handler(self, method, uri, auth, handler):
+        if not method in self.uri_handlers:
+            self.uri_handlers[method] = {}
+
+        if not uri in self.uri_handlers[method]:
+            self.uri_handlers[method][uri] = handler
+
+        reguri = http_pb2.HttpRegisterUri()
+        reguri.method = method
+        reguri.uri = uri
+        reguri.auth_required = auth
+
+        self.write_ext_packet("HTTPREGISTERURI", reguri)
+
+    def is_running(self):
+        return self.running
 
     def kill(self):
         self.bufferlock.acquire()
@@ -189,11 +218,39 @@ class KismetExternalInterface:
         pong.ping_seqno = seqno
         self.write_ext_packet("PONG", pong)
 
+    def handle_http_request(self, seqno, packet):
+        request = http_pb2.HttpRequest()
+        request.ParseFromString(packet)
+
+        if not request.method in self.uri_handlers:
+            raise RuntimeError("No URI handler registered for request {} {}".format(request.method, request.uri))
+
+        if not request.uri in self.uri_handlers[request.method]:
+            raise RuntimeError("No URI handler registered for request {} {}".format(request.method, request.uri))
+
+        self.uri_handlers[request.method][request.uri](self, request)
+
+    def send_http_response(self, req_id, data = "", resultcode = 200, stream = False, finished = True):
+        resp = http_pb2.HttpResponse()
+
+        # Set the response
+        resp.req_id = req_id
+
+        # Break the data into chunks and send each chunk as part of the response
+        for block in range(0, len(data), 1024):
+            resp.content = data[block:block+1024]
+            self.write_ext_packet("HTTPRESPONSE", resp)
+
+        # Do we finish it up?
+        if not stream or (stream and finished):
+            resp.content = ""
+            resp.resultcode = resultcode
+            resp.close_response = True
+            self.write_ext_packet("HTTPRESPONSE", resp)
+
     def handle_ping(self, seqno, packet):
         ping = kismet_pb2.Ping()
         ping.ParseFromString(packet)
-
-        print "PING: ", seqno
 
     def handle_pong(self, seqno, packet):
         pong = kismet_pb2.Pong()
