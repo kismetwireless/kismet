a3cb80af868e1ddbce26dac26169f066b94b8dfd
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-03-22 15:38:45 -0400

    Signedness fix

--- kis_external.cc
+++ kis_external.cc
@@ -137,7 +137,7 @@ void KisExternalInterface::BufferAvailable(size_t in_amt __attribute__((unused))
         frame_sz = data_sz + sizeof(kismet_external_frame);
 
         // If we'll never be able to read it, blow up
-        if (frame_sz >= ringbuf_handler->GetReadBufferAvailable()) {
+        if ((long int) frame_sz >= ringbuf_handler->GetReadBufferAvailable()) {
             ringbuf_handler->PeekFreeReadBufferData(buf);
 
             std::stringstream ss;
