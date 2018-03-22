4b898ab599260c0e6297a155b3aecc21a3f7fe01
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-03-22 15:11:10 -0400

    Started adding KismetLog module

---
+++ python_modules/KismetLog/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(name='KismetLog',
+      version='2018.0.0',
+      description='Kismet Database Log Helper Library',
+      author='Mike Kershaw / Dragorn',
+      author_email='dragorn@kismetwireless.net',
+      url='https://www.kismetwireless.net/',
+      install_requires=['sqlite],
+      packages=['KismetLog'],
+     )
