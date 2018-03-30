d5e4883f4bde43f073503b7ecafbba05189feb91
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-03-30 16:07:55 -0400

    Fix old ret val being used in tcpclient

--- tcpclient2.cc
+++ tcpclient2.cc
@@ -278,7 +278,7 @@ int TcpClientV2::Poll(fd_set& in_rset, fd_set& in_wset) {
         } else {
             // Consume whatever we managed to write
             handler->PeekFreeWriteBufferData(buf);
-            handler->ConsumeWriteBufferData(iret);
+            handler->ConsumeWriteBufferData(ret);
         }
     }
 
