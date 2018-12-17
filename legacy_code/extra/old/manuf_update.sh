26225cae065af1c55c35ca2bf2ae093bb9d669b2
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2018-12-16 22:36:26 -0500

    Junk old files

--- legacy_code/extra/old/manuf_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget http://www.unbolted.net/dump_clients.php -O ../conf/client_manuf
-wget http://www.unbolted.net/dump_aps.php -O ../conf/ap_manuf
-
--- legacy_code/extra/remote_wrapper.sh
+++
@@ -1,6 +0,0 @@
-while [ -f /tmp/kismet_remote/$1 ]; do 
-	/usr/local/bin/kismet_capture_tools/$1 --connect $2 --source $3
-	sleep 5;
-done
-
-print "/tmp/kismet_remote/$1 no longer present, stopping loop"
