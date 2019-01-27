368f782e7238b5efae8b2c02108cc35c849ec39e
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-01-26 23:39:15 -0500

    Start expanding filter

--- packet_filter.h
+++ packet_filter.h
@@ -38,6 +38,7 @@ public:
     __ProxyGet(filter_id, std::string, std::string, filter_id);
     __ProxyGet(filter_description, std::string, std::string, filter_description);
     __ProxyGet(filter_type, std::string, std::string, filter_type);
+    __Proxy(filter_default, uint8_t, bool, bool, filter_default);
 
     virtual bool filter_packet(kis_packet *packet) = 0;
 
@@ -48,6 +49,7 @@ protected:
         RegisterField("kismet.packetfilter.id", "Filter ID/Endpoint", &filter_id);
         RegisterField("kismet.packetfilter.description", "Filter description", &filter_description);
         RegisterField("kismet.packetfilter.type", "Filter mechanism", &filter_type);
+        RegisterField("kismet.packetfilter.default", "Default filter (pass/reject)", &filter_default);
     }
 
     kis_recursive_timed_mutex mutex;
@@ -55,13 +57,41 @@ protected:
     std::shared_ptr<TrackerElementString> filter_id;
     std::shared_ptr<TrackerElementString> filter_description;
     std::shared_ptr<TrackerElementString> filter_type;
+    std::shared_ptr<TrackerElementUInt8> filter_default;
+
+    // Default behavior endpoint
+    std::shared_ptr<Kis_Net_Httpd_Simple_Post_Endpoint> default_endp;
 };
 
+// Mac-address based filter.
+// Filters can be applied to 'ANY', source, destination, network, or transmitter
+// (in wifi terms, source, dest, bssid, or 4mac transmitter for wds).
+// Filters are true (filter/reject packet), or false (pass packet).  Packets not matched
+// by any filter are passed to the default filter term.
 class PacketfilterMacaddr : public Packetfilter {
 public:
     PacketfilterMacaddr(const std::string& in_id, const std::string& in_description);
     virtual ~PacketfilterMacaddr() {}
 
 
+protected:
+    virtual void register_fields() override {
+        tracker_component::register_fields();
 
+        RegisterField("kismet.packetfilter.macaddr.source", "Source addresses filtered", &filter_source);
+        RegisterField("kismet.packetfilter.macaddr.destination", "Destination addresses filtered", &filter_dest);
+        RegisterField("kismet.packetfilter.macaddr.network", "Network/BSSID addresses filtered", &filter_network);
+        RegisterField("kismet.packetfilter.macaddr.transmit", "Transmit addresses filtered", &filter_transmit);
+
+        RegisterField("kismet.packetfilter.macaddr.any", "Any matching address type", &filter_transmit);
+    }
+
+    // Source, dest, and BSSID (for wifi) or transmitter (for others)
+    std::shared_ptr<TrackerElementMacMap> filter_source;
+    std::shared_ptr<TrackerElementMacMap> filter_dest;
+    std::shared_ptr<TrackerElementMacMap> filter_network;
+    std::shared_ptr<TrackerElementMacMap> filter_transmit;
+
+    // ANY address found in a packet
+    std::shared_ptr<TrackerElementMacMap> filter_any;
 };
