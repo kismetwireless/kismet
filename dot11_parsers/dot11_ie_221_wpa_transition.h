4a77ad29c04e87297d8692a2b69ac2d636821a9f
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-04-09 11:12:07 -0400

    Add wpa owe transition tag parser

---
+++ dot11_parsers/dot11_ie_221_wpa_transition.h
@@ -0,0 +1,58 @@
+/*
+    This file is part of Kismet
+
+    Kismet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kismet is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kismet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __DOT11_IE_221_OWE_TRANSITION__
+#define __DOT11_IE_221_OWE_TRANSITION__ 
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <kaitai/kaitaistream.h>
+#include "multi_constexpr.h"
+#include "macaddr.h"
+
+class dot11_ie_221_owe_transition {
+public:
+    dot11_ie_221_owe_transition() { }
+    ~dot11_ie_221_owe_transition() { }
+
+    constexpr17 static unsigned int vendor_oui() {
+        return 0x506f9a;
+    }
+
+    void parse(std::shared_ptr<kaitai::kstream> p_io);
+
+    constexpr17 uint8_t vendor_type() const {
+        return m_vendor_type;
+    }
+
+    constexpr17 mac_addr bssid() const {
+        return m_bssid;
+    }
+
+    std::string ssid() const {
+        return m_ssid;
+    }
+
+protected:
+    uint8_t m_vendor_type;
+    mac_addr m_bssid;
+    std::string m_ssid;
+};
+
+#endif /* ifndef DOT11_IE_221_OWE_TRANSITION */
