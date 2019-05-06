2af1ec3da8175a7c0be614f187575c2fb7195f48
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-05-06 17:42:54 -0400

    Actually fix OWE display?

--- http_data/js/kismet.ui.dot11.js
+++ http_data/js/kismet.ui.dot11.js
@@ -636,10 +636,10 @@ kismet_ui.AddDeviceDetail("dot11", "Wi-Fi (802.11)", 0, {
                     title: "SSID",
                     render: function(opts) {
                         if (opts['value'] === '') {
-                            if (opts['base']['dot11.advertisedssid.owe_ssid'] == 0 || opts['base']['dot11.advertisedssid.owe_ssid'].length != 0) {
-                                return "<i>Unknown</i>";
-                            } else {
+                            if ('dot11.advertisedssid.owe_ssid' in opts['base']) {
                                 return "<i>SSID advertised as OWE</i>";
+                            } else {
+                                return "<i>Unknown / Cloaked</i>";
                             }
                         }
 
