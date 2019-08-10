82fefb50e2409cb7b8b89920a701f4e5fab672bd
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-08-10 15:41:47 -0400

    bulk snake_case replace DatasourceLinuxWifiBuilder to datasource_linux_wifi_builder

--- kismet_server.cc
+++ kismet_server.cc
@@ -875,7 +875,7 @@ int main(int argc, char *argv[], char *envp[]) {
     // Add the datasources
     datasourcetracker->register_datasource(shared_datasource_builder(new DatasourcePcapfileBuilder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_kismetdb_builder()));
-    datasourcetracker->register_datasource(shared_datasource_builder(new DatasourceLinuxWifiBuilder()));
+    datasourcetracker->register_datasource(shared_datasource_builder(new datasource_linux_wifi_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_linux_bluetooth_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new DatasourceOsxCorewlanWifiBuilder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new DatasourceRtl433Builder()));
