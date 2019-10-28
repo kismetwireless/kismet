005a773d6f1c077329fed9c0cca2ff735e6afc74
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-10-28 10:07:10 -0400

    Update tools for python3
    Update utility tools for python3

--- tools/compile_uav_conf.py
+++ tools/compile_uav_conf.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import yaml
 import os
 import sys
 
 if len(sys.argv) < 1:
-    print "Expected source yaml"
+    print("Expected source yaml")
     sys.exit(1)
 
-print sys.argv[1]
+print(sys.argv[1])
 
 with open(sys.argv[1], "r") as stream:
     data_loaded = yaml.load(stream)
@@ -17,8 +17,8 @@ with open(sys.argv[1], "r") as stream:
 for e in data_loaded:
     if 'header' in e:
         for l in e['header'].split("\n"):
-            print "# {}".format(l)
-        print
+            print("# {}".format(l))
+        print()
 
     if not 'uav' in e:
         continue
@@ -26,17 +26,17 @@ for e in data_loaded:
     uav = e['uav']
 
     if not 'id' in uav:
-        print "Expected 'id' in uav block, skipping: ", uav
+        print("Expected 'id' in uav block, skipping: ", uav)
         continue
 
     if not 'name' in uav:
-        print "Expected 'name' in uav block, skipping: ", uav
+        print("Expected 'name' in uav block, skipping: ", uav)
         continue
 
 
     if 'comment' in uav:
         for c in uav['comment'].split('\n'):
-            print "# {}".format(c)
+            print("# {}".format(c))
 
     uav["count"] = 1
     confstr = "uav_match={id}"
@@ -60,11 +60,11 @@ for e in data_loaded:
         
         for m in uav['mac']:
             uav['countmac'] = m
-            print confstr.format(**uav)
+            print(confstr.format(**uav))
             uav['count'] = uav['count'] + 1
     else:
-        print confstr.format(**uav)
+        print(confstr.format(**uav))
        
-    print
+    print()
 
 
