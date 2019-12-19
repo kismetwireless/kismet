c0c4eb1acc67dfe5587b84246d8475e446807046
Aaron Berger <code@bergera.com>
2019-12-19 14:43:43 -0600

    nrf51822 uuid fix

--- capture_nrf_51822/capture_nrf_51822.c
+++ capture_nrf_51822/capture_nrf_51822.c
@@ -81,7 +81,7 @@ int probe_callback(kis_capture_handler_t *caph, uint32_t seqno, char *definition
     if ((placeholder_len = cf_find_flag(&placeholder, "uuid", definition)) > 0) {
         *uuid = strndup(placeholder, placeholder_len);
     } else {
-        snprintf(errstr, STATUS_MAX, "%08X-0000-0000-0000-%12X",
+        snprintf(errstr, STATUS_MAX, "%08X-0000-0000-0000-%012X",
                 adler32_csum((unsigned char *) "kismet_cap_nrf_51822", 
                     strlen("kismet_cap_nrf_51822")) & 0xFFFFFFFF,
                 adler32_csum((unsigned char *) device,
@@ -141,15 +141,13 @@ int open_callback(kis_capture_handler_t *caph, uint32_t seqno, char *definition,
     if ((placeholder_len = cf_find_flag(&placeholder, "uuid", definition)) > 0) {
         *uuid = strndup(placeholder, placeholder_len);
     } else {
-        snprintf(errstr, STATUS_MAX, "%08X-0000-0000-0000-%12X",
+        snprintf(errstr, STATUS_MAX, "%08X-0000-0000-0000-%012X",
                 adler32_csum((unsigned char *) "kismet_cap_nrf_51822", 
                     strlen("kismet_cap_nrf_51822")) & 0xFFFFFFFF,
                 adler32_csum((unsigned char *) device,
                     strlen(device)));
         *uuid = strdup(errstr);
     }
-
-printf("uuid:%s\n",*uuid);
 
     /* open for r/w but no tty */
     localnrf->fd = open(device, O_RDWR | O_NOCTTY );
