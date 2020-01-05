5ff426d319ee565957591d24aa81c9cdc0797ba7
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2020-01-05 13:36:24 -0500

    Public def of string tokenizer
    Public def of string tokenizer

--- util.h
+++ util.h
@@ -161,6 +161,8 @@ std::string string_append(const std::string& s, const std::string& a, const std:
 int x_to_i(char x);
 int hex_to_uchar(unsigned char *in_hex, unsigned char *in_chr);
 
+
+std::vector<std::string> str_tokenize(std::string& in_str, const std::list<char>& in_split);
 std::vector<std::string> str_tokenize(const std::string& in_str, const std::string& in_split, 
         int return_partial = 1);
 std::string str_join(const std::vector<std::string>& in_content, const std::string& in_delim, 
@@ -188,8 +190,8 @@ std::vector<smart_word_token> base_str_tokenize(const std::string& in_str,
 std::vector<std::string> quote_str_tokenize(const std::string& in_str, const std::string& in_split);
 
 // Find a complete token w/in a string
-bool FindToken(const std::string& str, const std::string& needle, const std::string& split);
-bool FindToken(const std::string& str, const std::string& needle, const std::list<char>& splits);
+bool find_token(const std::string& str, const std::string& needle, const std::string& split);
+bool find_token(const std::string& str, const std::string& needle, const std::list<char>& splits);
 
 int TokenNullJoin(std::string *ret_str, const char **in_list);
 
