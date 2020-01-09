4b4cf22d50a720b681793b7744f02f1c3a8ee52e
bergera <code@bergera.com>
2020-01-09 15:30:14 -0600

    ticc2531 support

--- kismet_server.cc
+++ kismet_server.cc
@@ -76,6 +76,7 @@
 #include "datasource_nrf_51822.h"
 #include "datasource_ubertooth_one.h"
 #include "datasource_nxp_kw41z.h"
+#include "datasource_ti_cc_2531.h"
 
 #include "logtracker.h"
 #include "kis_ppilogfile.h"
@@ -891,6 +892,7 @@ int main(int argc, char *argv[], char *envp[]) {
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_nrf51822_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_ubertooth_one_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_nxpkw41z_builder()));
+    datasourcetracker->register_datasource(shared_datasource_builder(new datasource_ticc2531_builder()));
 
     // Create the database logger as a global because it's a special case
     kis_database_logfile::create_kisdatabaselog();
