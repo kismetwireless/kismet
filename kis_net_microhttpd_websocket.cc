73f651a9eb46ade40c80c6fd1affad5fb560486e
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2020-02-03 18:07:09 -0500

    Minor work on websockets
    Minor work on websockets

--- kis_net_microhttpd_websocket.cc
+++ kis_net_microhttpd_websocket.cc
@@ -93,7 +93,26 @@ int kis_net_httpd_websocket_pollable::pollable_merge_set(int in_max_fd, fd_set *
 }
 
 int kis_net_httpd_websocket_pollable::pollable_poll(fd_set& in_rset, fd_set& in_wset) {
-    // Read from the socket and decode websocket-isms, etc
+    /*
+       0                   1                   2                   3
+       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-------+-+-------------+-------------------------------+
+       |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+       |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+       |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+       | |1|2|3|       |K|             |                               |
+       +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+       |     Extended payload length continued, if payload len == 127  |
+       + - - - - - - - - - - - - - - - +-------------------------------+
+       |                               |Masking-key, if MASK set to 1  |
+       +-------------------------------+-------------------------------+
+       | Masking-key (continued)       |          Payload Data         |
+       +-------------------------------- - - - - - - - - - - - - - - - +
+       :                     Payload Data continued ...                :
+       + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+       |                     Payload Data continued ...                |
+       +---------------------------------------------------------------+
+       */
 
     return 0;
 }
