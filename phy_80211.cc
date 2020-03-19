04aaeb08422c344cf583270954e4b06c4aa22fd3
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2020-03-19 16:14:42 -0400

    Minor formatting tweaks
    Minor formatting tweaks

--- phy_80211.cc
+++ phy_80211.cc
@@ -396,14 +396,14 @@ kis_80211_phy::kis_80211_phy(global_registry *in_globalreg, int in_phyid) :
                 "incorrect length may indicate an attempted buffer overflow attack.  "
                 "Specific attacks have their own alerts; this indicates a general, but "
                 "otherwise unknown, malformed tag.");
-	alert_rtlwifi_p2p_ref =
-		alertracker->activate_configured_alert("RTLWIFIP2P",
-				"A bug in the Linux RTLWIFI P2P parsers could result in a crash "
-				"or potential code execution due to malformed notification of "
-				"absence records, as detailed in CVE-2019-17666");
+    alert_rtlwifi_p2p_ref =
+        alertracker->activate_configured_alert("RTLWIFIP2P",
+                "A bug in the Linux RTLWIFI P2P parsers could result in a crash "
+                "or potential code execution due to malformed notification of "
+                "absence records, as detailed in CVE-2019-17666");
     alert_deauthflood_ref =
         alertracker->activate_configured_alert("DEAUTHFLOOD",
-                "By spoofing disassociate and deauthenticate packets, an attacker "
+                "By spoofing disassociate or deauthenticate packets, an attacker "
                 "may disconnect clients from a network which does not support "
                 "management frame protection (MFP); This can be used to cause a "
                 "denial of service or to disconnect clients in an attempt to "
