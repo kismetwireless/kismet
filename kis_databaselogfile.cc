kismetwireless/kismet

commit 6f98966a1ae3d45a9d4b5da9fa129eaedefb4c31

return error on kismetdb pcap when kismetdb not enabled

kis_databaselogfile.cc
@@ -1006,6 +1006,11 @@ int KisDatabaseLogfile::Httpd_CreateStreamResponse(Kis_Net_Httpd *httpd,
     }
 
     if (stripped.find("/logging/kismetdb/pcap/") == 0 && suffix == "pcapng") {
+        if (db == nullptr || db_enabled) {
+            connection->httpcode = 500;
+            return MHD_YES;
+        }
+
         using namespace kissqlite3;
         auto query = _SELECT(db, "packets", {"ts_sec", "ts_usec", "datasource", "dlt", "packet"});
 
@@ -1158,6 +1163,11 @@ int KisDatabaseLogfile::Httpd_PostComplete(Kis_Net_Httpd_Connection *concls) {
     }
 
     if (stripped.find("/logging/kismetdb/pcap/") == 0 && suffix == "pcapng") {
+        if (db == nullptr || db_enabled) {
+            concls->httpcode = 500;
+            return MHD_YES;
+        }
+
         try {
             if (concls->variable_cache.find("json") != 
                     concls->variable_cache.end()) {
@@ -1206,13 +1216,16 @@ int KisDatabaseLogfile::Httpd_PostComplete(Kis_Net_Httpd_Connection *concls) {
     if (filterdata != nullptr) {
         try {
             if (filterdata->hasKey("timestamp_start")) 
-                query.append_where(AND, _WHERE("ts_sec", GE, filterdata->getKeyAsNumber("timestamp_start")));
+                query.append_where(AND, 
+                        _WHERE("ts_sec", GE, filterdata->getKeyAsNumber("timestamp_start")));
 
             if (filterdata->hasKey("timestamp_end")) 
-                query.append_where(AND, _WHERE("ts_sec", LE, filterdata->getKeyAsNumber("timestamp_end")));
+                query.append_where(AND, 
+                        _WHERE("ts_sec", LE, filterdata->getKeyAsNumber("timestamp_end")));
 
             if (filterdata->hasKey("datasource")) 
-                query.append_where(AND, _WHERE("datasource", LIKE, filterdata->getKeyAsString("datasource")));
+                query.append_where(AND, 
+                        _WHERE("datasource", LIKE, filterdata->getKeyAsString("datasource")));
 
             if (filterdata->hasKey("device_id")) 
                 query.append_where(AND, _WHERE("devkey", LIKE, filterdata->getKeyAsString("device_id")));
@@ -1221,13 +1234,16 @@ int KisDatabaseLogfile::Httpd_PostComplete(Kis_Net_Httpd_Connection *concls) {
                 query.append_where(AND, _WHERE("dlt", EQ, filterdata->getKeyAsNumber("dlt")));
 
             if (filterdata->hasKey("frequency")) 
-                query.append_where(AND, _WHERE("frequency", EQ, filterdata->getKeyAsNumber("frequency")));
+                query.append_where(AND, 
+                        _WHERE("frequency", EQ, filterdata->getKeyAsNumber("frequency")));
 
             if (filterdata->hasKey("frequency_min")) 
-                query.append_where(AND, _WHERE("frequency", GE, filterdata->getKeyAsNumber("frequency_min")));
+                query.append_where(AND, 
+                        _WHERE("frequency", GE, filterdata->getKeyAsNumber("frequency_min")));
 
             if (filterdata->hasKey("frequency_max")) 
-                query.append_where(AND, _WHERE("frequency", LE, filterdata->getKeyAsNumber("frequency_max")));
+                query.append_where(AND, 
+                        _WHERE("frequency", LE, filterdata->getKeyAsNumber("frequency_max")));
 
             /*
             if (filterdata->hasKey("channel")) 
@@ -1241,25 +1257,32 @@ int KisDatabaseLogfile::Httpd_PostComplete(Kis_Net_Httpd_Connection *concls) {
                 query.append_where(AND, _WHERE("signal", LE, filterdata->getKeyAsNumber("signal_max")));
 
             if (filterdata->hasKey("address_source")) 
-                query.append_where(AND, _WHERE("sourcemac", LIKE, filterdata->getKeyAsString("address_source")));
+                query.append_where(AND, 
+                        _WHERE("sourcemac", LIKE, filterdata->getKeyAsString("address_source")));
 
             if (filterdata->hasKey("address_dest")) 
-                query.append_where(AND, _WHERE("destmac", LIKE, filterdata->getKeyAsString("address_dest")));
+                query.append_where(AND, 
+                        _WHERE("destmac", LIKE, filterdata->getKeyAsString("address_dest")));
 
             if (filterdata->hasKey("address_trans")) 
-                query.append_where(AND, _WHERE("transmac", LIKE, filterdata->getKeyAsString("address_trans")));
+                query.append_where(AND, 
+                        _WHERE("transmac", LIKE, filterdata->getKeyAsString("address_trans")));
 
             if (filterdata->hasKey("location_lat_min"))
-                query.append_where(AND, _WHERE("lat", GE, filterdata->getKeyAsNumber("location_lat_min") * 100000));
+                query.append_where(AND, 
+                        _WHERE("lat", GE, filterdata->getKeyAsNumber("location_lat_min") * 100000));
 
             if (filterdata->hasKey("location_lon_min"))
-                query.append_where(AND, _WHERE("lon", GE, filterdata->getKeyAsNumber("location_lon_min") * 100000));
+                query.append_where(AND, 
+                        _WHERE("lon", GE, filterdata->getKeyAsNumber("location_lon_min") * 100000));
 
             if (filterdata->hasKey("location_lat_max"))
-                query.append_where(AND, _WHERE("lat", LE, filterdata->getKeyAsNumber("location_lat_max") * 100000));
+                query.append_where(AND, 
+                        _WHERE("lat", LE, filterdata->getKeyAsNumber("location_lat_max") * 100000));
 
             if (filterdata->hasKey("location_lon_max"))
-                query.append_where(AND, _WHERE("lon", LE, filterdata->getKeyAsNumber("location_lon_max") * 100000));
+                query.append_where(AND, 
+                        _WHERE("lon", LE, filterdata->getKeyAsNumber("location_lon_max") * 100000));
 
             if (filterdata->hasKey("size_min"))
                 query.append_where(AND, _WHERE("size", GE, filterdata->getKeyAsNumber("size_min")));

commit ad156dae6b33cfc5f03df4f794a314d6fa5d7542

kis_databaselogfile.cc: fix GET/POST requests
This commit contains two small fixes:
 - Check for !db_enabled instead of db_enabled to allow the user to
   correctly retrieve the packets from kismet db (broken since commit
   6f98966a1ae3d45a9d4b5da9fa129eaedefb4c31)
 - Replace size_max or size by packet_len to allow the user to specify a
   minimum or maximum packet size

It should be noted that there is still an issue with the current API:
the microseconds given by the user in timestamp_start or timestamp_end
are ignored because tv_usec is never checked. This is not easy to fix
with the current sqlite wrapper that does not to allow the following SQL
statement:
   WHERE (tv_sec GT x OR tv_sec EQ x AND tv_usec GE y) AND (a EQ b)

Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

kis_databaselogfile.cc
@@ -1389,7 +1389,7 @@ int kis_database_logfile::httpd_create_stream_response(kis_net_httpd *httpd,
     }
 
     if (stripped.find("/logging/kismetdb/pcap/") == 0 && suffix == "pcapng") {
-        if (db == nullptr || db_enabled) {
+        if (db == nullptr || !db_enabled) {
             connection->httpcode = 500;
             return MHD_YES;
         }
@@ -1475,11 +1475,11 @@ int kis_database_logfile::httpd_create_stream_response(kis_net_httpd *httpd,
                             connection->variable_cache_as<double>("location_lon_max")));
 
             if (connection->has_cached_variable("size_min"))
-                query.append_where(AND, _WHERE("size", GE, 
+                query.append_where(AND, _WHERE("packet_len", GE, 
                             connection->variable_cache_as<long int>("size_min")));
 
             if (connection->has_cached_variable("size_max"))
-                query.append_where(AND, _WHERE("size_max", LE, 
+                query.append_where(AND, _WHERE("packet_len", LE, 
                             connection->variable_cache_as<long int>("size_max")));
 
             if (connection->has_cached_variable("limit"))
@@ -1546,7 +1546,7 @@ int kis_database_logfile::httpd_post_complete(kis_net_httpd_connection *concls) 
     }
 
     if (stripped.find("/logging/kismetdb/pcap/") == 0 && suffix == "pcapng") {
-        if (db == nullptr || db_enabled) {
+        if (db == nullptr || !db_enabled) {
             concls->httpcode = 500;
             return MHD_YES;
         }
@@ -1668,10 +1668,10 @@ int kis_database_logfile::httpd_post_complete(kis_net_httpd_connection *concls) 
                         _WHERE("lon", LE, filterdata->key_as_number("location_lon_max")));
 
             if (filterdata->has_key("size_min"))
-                query.append_where(AND, _WHERE("size", GE, filterdata->key_as_number("size_min")));
+                query.append_where(AND, _WHERE("packet_len", GE, filterdata->key_as_number("size_min")));
 
             if (filterdata->has_key("size_max"))
-                query.append_where(AND, _WHERE("size_max", LE, filterdata->key_as_number("size_max")));
+                query.append_where(AND, _WHERE("packet_len", LE, filterdata->key_as_number("size_max")));
 
             if (filterdata->has_key("limit"))
                 query.append_clause(LIMIT, filterdata->key_as_number("limit"));