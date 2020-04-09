kismetwireless/kismet

commit 616930b6a1c9f91ff851dbc1c9f076b80224d491

Remove included msgpack Remove built-in msgpack serialization

gpsweb.cc
@@ -20,7 +20,6 @@
 #include "gpsweb.h"
 #include "gpstracker.h"
 #include "messagebus.h"
-#include "msgpack_adapter.h"
 
 // Don't bind to the http server until we're created, so pass a null to
 // the stream_handler init
@@ -115,6 +114,7 @@ int GPSWeb::Httpd_PostIterator(void *coninfo_cls, enum MHD_ValueKind kind,
     bool set_alt = false, set_spd = false;
 
     if (concls->url == "/gps/web/update.cmd") {
+#if 0
         if (strcmp(key, "msgpack") == 0 && size > 0) {
             std::string decode = Base64::decode(std::string(data));
 
@@ -160,6 +160,10 @@ int GPSWeb::Httpd_PostIterator(void *coninfo_cls, enum MHD_ValueKind kind,
                 return 1;
             }
         }
+#endif
+        concls->response_stream << "Being rewritten";
+        concls->httpcode = 400;
+        return 1;
     }
 
     // If we didn't handle it and got here, we don't know what it is, throw an

commit 089b639bfd736a54988a8f73442100ba15c79960

Fix Web GPS
Web GPS is broken since commit 616930b6a1c9f91ff851dbc1c9f076b80224d491
which removed msgack. Add it back as it is still documented as a valid
API in https://www.kismetwireless.net/docs/readme/gps

Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

conf/kismet.conf
@@ -158,6 +158,7 @@ override_remote_timestamp=true
 # gps=tcp:host=1.2.3.4,port=4352
 # gps=gpsd:host=localhost,port=2947
 # gps=virtual:lat=123.45,lon=45.678,alt=1234
+# gps=web:name=gpsweb
 
 
 

gpsweb.cc
@@ -19,6 +19,7 @@
 #include "base64.h"
 #include "gpsweb.h"
 #include "gpstracker.h"
+#include "kismet_json.h"
 #include "messagebus.h"
 
 // Don't bind to the http server until we're created, so pass a null to
@@ -95,12 +96,7 @@ void kis_gps_web::httpd_create_stream_response(kis_net_httpd *httpd,
     return;
 }
 
-int kis_gps_web::httpd_post_iterator(void *coninfo_cls, enum MHD_ValueKind kind, 
-        const char *key, const char *filename, const char *content_type,
-        const char *transfer_encoding, const char *data, 
-        uint64_t off, size_t size) {
-
-    kis_net_httpd_connection *concls = (kis_net_httpd_connection *) coninfo_cls;
+int kis_gps_web::httpd_post_complete(kis_net_httpd_connection *concls) {
 
     bool handled = false;
 
@@ -113,56 +109,35 @@ int kis_gps_web::httpd_post_iterator(void *coninfo_cls, enum MHD_ValueKind kind,
 
     double lat = 0, lon = 0, alt = 0, spd = 0;
     bool set_alt = false, set_spd = false;
+    shared_structured structdata;
 
-    if (concls->url == "/gps/web/update.cmd") {
-#if 0
-        if (strcmp(key, "msgpack") == 0 && size > 0) {
-            std::string decode = base64::decode(std::string(data));
-
-            // Get the dictionary
-            MsgpackAdapter::MsgpackStrMap params;
-            MsgpackAdapter::MsgpackStrMap::iterator obj_iter;
-            msgpack::unpacked result;
-
-            try {
-                msgpack::unpack(result, decode.data(), decode.size());
-                msgpack::object deserialized = result.get();
-                params = deserialized.as<MsgpackAdapter::MsgpackStrMap>();
-
-                // Lat and lon are required
-                obj_iter = params.find("lat");
-                if (obj_iter == params.end())
-                    throw std::runtime_error("expected 'lat' entry");
-                lat = obj_iter->second.as<double>();
-
-                obj_iter = params.find("lon");
-                if (obj_iter == params.end())
-                    throw std::runtime_error("expected 'lon' entry");
-                lon = obj_iter->second.as<double>();
-
-                // Alt and speed are optional, but if one is provided,
-                // it needs to be a double
-                obj_iter = params.find("alt");
-                if (obj_iter != params.end()) {
-                    alt = obj_iter->second.as<double>();
-                    set_alt = true;
-                }
-
-                obj_iter = params.find("spd");
-                if (obj_iter != params.end()) {
-                    spd = obj_iter->second.as<double>();
-                    set_spd = true;
-                }
-
-                handled = true;
-            } catch (const std::exception& e) {
-                concls->response_stream << "Invalid request " << e.what();
-                concls->httpcode = 400;
-                return 1;
-            }
+    try {
+        if (concls->variable_cache.find("json") != concls->variable_cache.end()) {
+            structdata.reset(new structured_json(concls->variable_cache["json"]->str()));
+        } else {
+            throw std::runtime_error("could not find data");
         }
-#endif
-        concls->response_stream << "Being rewritten";
+
+        if (concls->url == "/gps/web/update.cmd") {
+            // Lat and lon are required
+            lat = structdata->key_as_number("lat");
+            lon = structdata->key_as_number("lon");
+
+            // Alt and speed are optional
+            if (structdata->has_key("alt")) {
+                alt = structdata->key_as_number("alt");
+                set_alt = true;
+            }
+
+            if (structdata->has_key("spd")) {
+                spd = structdata->key_as_number("spd");
+                set_spd = true;
+            }
+
+            handled = true;
+        }
+    } catch (const std::exception& e) {
+        concls->response_stream << "Invalid request " << e.what();
         concls->httpcode = 400;
         return 1;
     }

gpsweb.h
@@ -49,10 +49,7 @@ public:
             const char *url, const char *method, const char *upload_data,
             size_t *upload_data_size, std::stringstream &stream);
 
-    virtual int httpd_post_iterator(void *coninfo_cls, enum MHD_ValueKind kind, 
-            const char *key, const char *filename, const char *content_type,
-            const char *transfer_encoding, const char *data, 
-            uint64_t off, size_t size);
+    virtual int httpd_post_complete(kis_net_httpd_connection *concls);
 
 protected:
     // Last time we calculated the heading, don't do it more than once every 