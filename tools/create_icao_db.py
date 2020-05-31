e41cd8ddce45b08f3517f66247ff0295540818cd
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2020-05-31 13:39:42 -0400

    Add atype to icao generation
    Add atype to icao generation

--- tools/create_icao_db.py
+++ tools/create_icao_db.py
@@ -3,7 +3,7 @@
 # Downloads http://registry.faa.gov/database/ReleasableAircraft.zip and extracts it in ram
 # to generate the aircraft ICAO database.
 # 
-# Primarily used to during kismet release tagging to generate the aircraft db
+# Used during Kismet release tagging to generate the aircraft db
 
 import csv
 import urllib.request
@@ -15,6 +15,7 @@ import sys
 
 acft={}
 mdl={}
+res={}
 
 with urllib.request.urlopen("http://registry.faa.gov/database/ReleasableAircraft.zip") as response, io.BytesIO() as mem_zf:
     # Copy into an in-memory zipfile
@@ -23,6 +24,12 @@ with urllib.request.urlopen("http://registry.faa.gov/database/ReleasableAircraft
 
     # open as a zip
     zipf = zipfile.ZipFile(mem_zf)
+
+    with io.TextIOWrapper(zipf.open('RESERVED.txt', 'r')) as csvfile:
+        reserved = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reserved, None)
+        for row in reserved:
+            res[row[0]] = row[1]
 
     with io.TextIOWrapper(zipf.open('ACFTREF.txt', 'r')) as csvfile:
         aircraft = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -40,5 +47,22 @@ with urllib.request.urlopen("http://registry.faa.gov/database/ReleasableAircraft
 
         for row in airplanes:
             num_rows = num_rows + 1
-            print(row[33].rstrip().lower()+'\t'+row[0]+'\t'+mdl[row[2]]+'\t"' + acft[row[2]] + '"\t"' +row[6].rstrip()+'"')
+
+            # ICAO, CALL, TYPE, MODEL, OWNER, ATYPE
+            if row[0] in res.keys():
+                print("{}\t{}\t{}\t\"{}\"\t\"{}\"\t{}".format(
+                    row[33].rstrip().lower(),
+                    row[0],
+                    mdl[row[2]],
+                    acft[row[2]],
+                    res[row[0]].rstrip(),
+                    row[18]))
+            else:
+                print("{}\t{}\t{}\t\"{}\"\t\"{}\"\t{}".format(
+                    row[33].rstrip().lower(),
+                    row[0],
+                    mdl[row[2]],
+                    acft[row[2]],
+                    row[6].rstrip(),
+                    row[18]))
 
