d326056bbd7d86871a01f1ecaa8531cf2f85872e
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2020-08-28 16:36:11 -0400

    Fix improper logic on signal threshold for location
    Fix improper logic on signal threshold for location

--- devicetracker.cc
+++ devicetracker.cc
@@ -869,8 +869,9 @@ std::shared_ptr<kis_tracked_device_base>
     if (((in_flags & UCD_UPDATE_LOCATION) ||
                 ((in_flags & UCD_UPDATE_EMPTY_LOCATION) && !device->has_location_cloud())) &&
             pack_gpsinfo != NULL &&
-            (device_location_signal_threshold != 0 && pack_l1info != NULL &&
-             pack_l1info->signal_dbm >= device_location_signal_threshold)) {
+            (device_location_signal_threshold == 0 || 
+             ( device_location_signal_threshold != 0 && pack_l1info != NULL &&
+             pack_l1info->signal_dbm >= device_location_signal_threshold))) {
         device->get_location()->add_loc(pack_gpsinfo->lat, pack_gpsinfo->lon,
                 pack_gpsinfo->alt, pack_gpsinfo->fix, pack_gpsinfo->speed,
                 pack_gpsinfo->heading);
