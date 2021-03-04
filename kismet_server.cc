ef676ff70a0d1fe5560cf0a013fdc1221f23cf35
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2021-03-03 19:54:06 -0500

    Enable adsb proxy, but source driver not complete
    Enable adsb proxy, but source driver not complete

--- kismet_server.cc
+++ kismet_server.cc
@@ -81,6 +81,7 @@
 #include "datasource_dot11_scan.h"
 #include "datasource_bluetooth_scan.h"
 #include "datasource_bladerf_wiphy.h"
+#include "datasource_adsbproxy.h"
 
 #include "logtracker.h"
 #include "kis_ppilogfile.h"
@@ -898,6 +899,7 @@ int main(int argc, char *argv[], char *envp[]) {
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_nxpkw41z_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_ticc2531_builder()));
     datasourcetracker->register_datasource(shared_datasource_builder(new datasource_bladerf_wiphy_builder()));
+    datasourcetracker->register_datasource(shared_datasource_builder(new datasource_adsbproxy_builder()));
 
     // Virtual sources get a special meta-builder
     datasource_virtual_builder::create_virtualbuilder();
