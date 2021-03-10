8e24f10b9a82127286b59d4246bd78f61a42ce11
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2021-03-10 08:02:57 -0500

    More fixes for older compilers
    More fixes for older compilers

--- phy_bluetooth.cc
+++ phy_bluetooth.cc
@@ -158,8 +158,10 @@ int kis_bluetooth_phy::packet_bluetooth_scan_json_classifier(CHAINCALL_PARMS) {
                      UCD_UPDATE_SEENBY | UCD_UPDATE_ENCRYPTION),
                     "Bluetooth Device");
 
-        kis_unique_lock lk_list(btphy->devicetracker->get_devicelist_mutex(), std::defer_lock, "packet_bluetooth_scan_classifier");
-        kis_unique_lock lk_device(btdev->device_mutex, std::defer_lock, "packet_bluetooth_scan_classifier");
+        kis_unique_lock<kis_mutex> lk_list(btphy->devicetracker->get_devicelist_mutex(), 
+                std::defer_lock, "packet_bluetooth_scan_classifier");
+        kis_unique_lock<kis_mutex> lk_device(btdev->device_mutex, std::defer_lock, 
+                "packet_bluetooth_scan_classifier");
         std::lock(lk_list, lk_device);
 
         // Mapped to base name
@@ -263,8 +265,10 @@ int kis_bluetooth_phy::packet_tracker_bluetooth(CHAINCALL_PARMS) {
     if (basedev == nullptr)
         return 0;
 
-    kis_unique_lock lk_list(btphy->devicetracker->get_devicelist_mutex(), std::defer_lock, "packet_tracker_bluetooth");
-    kis_unique_lock lk_device(basedev->device_mutex, std::defer_lock, "packet_tracker_bluetooth");
+    kis_unique_lock<kis_mutex> lk_list(btphy->devicetracker->get_devicelist_mutex(), 
+            std::defer_lock, "packet_tracker_bluetooth");
+    kis_unique_lock<kis_mutex> lk_device(basedev->device_mutex, std::defer_lock, 
+            "packet_tracker_bluetooth");
     std::lock(lk_list, lk_device);
 
 
--- phy_nrf_mousejack.cc
+++ phy_nrf_mousejack.cc
@@ -143,8 +143,10 @@ int Kis_Mousejack_Phy::CommonClassifierMousejack(CHAINCALL_PARMS) {
                  UCD_UPDATE_SEENBY | UCD_UPDATE_ENCRYPTION),
                 "KB/Mouse");
 
-    kis_unique_lock lk_list(mphy->devicetracker->get_devicelist_mutex(), std::defer_lock, "common_classifier_mousejack");
-    kis_unique_lock lk_device(device->device_mutex, std::defer_lock, "common_classifier_mousejack");
+    kis_unique_lock<kis_mutex> lk_list(mphy->devicetracker->get_devicelist_mutex(), 
+            std::defer_lock, "common_classifier_mousejack");
+    kis_unique_lock<kis_mutex> lk_device(device->device_mutex, std::defer_lock, 
+            "common_classifier_mousejack");
     std::lock(lk_list, lk_device);
 
     // Figure out what we think it could be; this isn't very precise.  Fingerprinting
