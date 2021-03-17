56b2101683a46f8ce07505ec4d26d145aa14a17c
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2021-03-16 20:26:44 -0400

    don't pass shared pointers as references
    don't pass shared pointers as references

--- devicetracker_view.cc
+++ devicetracker_view.cc
@@ -160,7 +160,7 @@ device_tracker_view::device_tracker_view(const std::string& in_id, const std::st
                                 // serializes them with the fields record
                                 auto tid = 
                                     timetracker->register_timer(std::chrono::seconds(rate), true,
-                                            [this, con, dev_r, dev_k, dev_m, json, &ws, &last_tm, rename_map, format_t](int) -> int {
+                                            [this, con, dev_r, dev_k, dev_m, json, ws, &last_tm, rename_map, format_t](int) -> int {
                                                 if (dev_r == "*") {
                                                     auto worker = device_tracker_view_function_worker([json, last_tm, format_t, ws](std::shared_ptr<kis_tracked_device_base> dev) -> bool {
                                                         if (dev->get_mod_time() > last_tm) {
