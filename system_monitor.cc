8a7e739be4ebb34deb8abdd35c47514757d8ce9d
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2021-11-07 19:30:59 -0500

    Check for valid fetch of temp on rpi before using it
    Check for valid fetch of temp on rpi before using it

--- system_monitor.cc
+++ system_monitor.cc
@@ -408,11 +408,11 @@ int Systemmonitor::timetracker_event(int eventid) {
         std::string synth_name = "system-thermal-0";
         double temp;
 
-        fscanf(tempf, "%lf", &temp);
+        if (fscanf(tempf, "%lf", &temp) == 1) {
+            temp = temp / 1000;
+            status->get_sensors_fans()->insert(synth_name, std::make_shared<tracker_element_double>(0, temp));
+        }
 
-        temp = temp / 1000;
-
-        status->get_sensors_fans()->insert(synth_name, std::make_shared<tracker_element_double>(0, temp));
 
         fclose(tempf);
     }
