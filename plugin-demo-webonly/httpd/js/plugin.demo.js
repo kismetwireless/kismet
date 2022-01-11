9fdbc90aa93fede8bad7aa5dea4ddb6284493a35
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-01-11 13:02:09 -0500

    Update demo plugin for new loading method
    Update demo plugin for new loading method

--- plugin-demo-webonly/httpd/js/plugin.demo.js
+++ plugin-demo-webonly/httpd/js/plugin.demo.js
@@ -1,21 +1,10 @@
-(
-  typeof define === "function" ? function (m) { define("plugin-web-js", m); } :
-  typeof exports === "object" ? function (m) { module.exports = m(); } :
-  function(m){ this.plugin_web = m(); }
-)(function () {
 
 "use strict";
 
-var exports = {};
-
-// Flag we're still loading
-exports.load_complete = 0;
+export const SomeFunction = () => {
+    console.log("some exported function from demo plugin");
+}
 
 console.log("Loaded demo plugin");
 
-// We're done loading
-exports.load_complete = 1;
 
-return exports;
-
-});
