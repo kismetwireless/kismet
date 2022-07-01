262b1e591c00dfd69114f13209b01d5b7e4b7696
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-07-01 17:12:09 -0400

    PacketUI: Add packet processing rate to UI display (actual packets handled rate)

--- http_data/js/kismet.ui.base.js
+++ http_data/js/kismet.ui.base.js
@@ -1518,12 +1518,22 @@ function packetqueuedisplay_refresh() {
             kismet.RecalcRrdData2(data['kismet.packetchain.packets_rrd'], rrdtype);
         var queue_linedata =
             kismet.RecalcRrdData2(data['kismet.packetchain.queued_packets_rrd'], rrdtype);
+        var processing_linedata =
+            kismet.RecalcRrdData2(data['kismet.packetchain.processed_packets_rrd'], rrdtype);
         var drop_linedata =
             kismet.RecalcRrdData2(data['kismet.packetchain.dropped_packets_rrd'], rrdtype);
         var dupe_linedata =
             kismet.RecalcRrdData2(data['kismet.packetchain.dupe_packets_rrd'], rrdtype);
 
         var datasets = [
+            {
+                label: 'Processed',
+                fill: 'false',
+                borderColor: 'orange',
+                backgroundColor: 'transparent',
+                data: processing_linedata,
+                pointStyle: 'cross',
+            },
             {
                 label: 'Incoming packets (peak)',
                 fill: 'false',
@@ -1540,7 +1550,7 @@ function packetqueuedisplay_refresh() {
                 pointStyle: 'rect',
             },
             {
-                label: 'Processing queue',
+                label: 'Queue',
                 fill: 'false',
                 borderColor: 'blue',
                 backgroundColor: 'transparent',
