9eac87b15ed67297d5cfb242c200d2546ec9a1ed
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-08-12 10:21:14 -0400

    phy80211: Update channel translation for 6ghz channels

--- phy_80211.cc
+++ phy_80211.cc
@@ -1054,18 +1054,22 @@ const std::string kis_80211_phy::khz_to_channel(const double in_khz) {
         throw std::runtime_error("invalid freq");
 
     int mhz = in_khz / 1000;
-
-    if (mhz == 2484)
-        return "14";
-    else if (mhz < 2484)
-        return fmt::format("{}", (mhz - 2407) / 5);
-    else if (mhz >= 4910 && mhz <= 4980)
-        return fmt::format("{}", (mhz - 4000) / 5);
-    else if (mhz <= 45000)
-        return fmt::format("{}", (mhz - 5000) / 5);
-    else if (mhz >= 58320 && mhz <= 64800)
-        return fmt::format("{}", (mhz - 56160) / 2160);
-    else
+ 
+	if (mhz == 2484)
+		return "14";
+	else if (mhz == 5935)
+		return "2";
+	else if (mhz < 2484)
+		return fmt::format("{}", (mhz - 2407) / 5);
+	else if (mhz >= 4910 && mhz <= 4980)
+		return fmt::format("{}", (mhz - 4000) / 5);
+	else if (mhz < 5950)
+		return fmt::format("{}", (mhz - 5000) / 5);
+	else if (mhz <= 45000) /* DMG band lower limit */
+		return fmt::format("{}", (mhz - 5950) / 5);
+	else if (mhz >= 58320 && mhz <= 70200)
+		return fmt::format("{}", (mhz - 56160) / 2160);
+	else
         return fmt::format("{}", mhz);
 }
 
