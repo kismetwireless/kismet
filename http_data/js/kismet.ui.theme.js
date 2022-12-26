f3ebf8c5f06a484030ea539e03b1ec6486d854f7
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-12-25 22:01:03 -0500

    themes: Add missing combined file

---
+++ http_data/js/kismet.ui.theme.js
@@ -0,0 +1,58 @@
+/* jshint browser: true */
+/* global define, module */
+( // Module boilerplate to support browser globals and browserify and AMD.
+  typeof define === "function" ? function (m) { define("kismet_theme", m); } :
+  typeof exports === "object" ? function (m) { module.exports = m(); } :
+  function(m){ this.kismet_theme = m(); }
+)(function () {
+    "use strict";
+
+    var exports = {};
+
+
+    var storage = Storages.localStorage;
+    if (!storage.isSet('kismet.ui.theme') || (storage.isSet('kismet.ui.theme') && 
+        storage.get('kismet.ui.theme') == 'dark')) { 
+
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/kismet-dark.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/dark.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/datatables-dark.css">');
+
+        exports.theme = 'dark';
+
+        exports.page_background = '#222';
+
+        exports.sparkline_main = '#FFF';
+        exports.sparkline_multi_a = '#F7FF00';
+        exports.sparkline_multi_b = '#00FF00';
+
+        Chart.defaults.backgroundColor = '#333';
+        Chart.defaults.borderColor = '#555';
+        Chart.defaults.color = '#FFF';
+
+        exports.graphBasicColor = 'rgba(255, 255, 255, 1)';
+        exports.graphBasicBackgroundColor = 'rgba(200, 200, 200, 0.33)',
+
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else { 
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/light.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/datatables.min.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/kismet.css">');
+
+        exports.theme = 'light';
+
+        exports.page_background = '#FFF';
+        exports.sparkline_multi_a = '#C70039';
+        exports.sparkline_multi_b = '#002EFF';
+
+        exports.sparkline_main = '#000';
+
+        exports.graphBasicColor = 'rgba(160, 160, 160, 1)';
+        exports.graphBasicBackgroundColor = 'rgba(100, 100, 100, 0.33)';
+
+        // document.documentElement.setAttribute('data-theme', 'light');
+    }
+
+    return exports;
+});
