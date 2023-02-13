cab7b27db7e74dbd0dbc5a67180a1cbbb7469239
pyllyukko <pyllyukko@maimed.org>
2023-02-13 22:12:35 +0200

    Fixed compile_uav_conf.py
    https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- tools/compile_uav_conf.py
+++ tools/compile_uav_conf.py
@@ -11,7 +11,7 @@ if len(sys.argv) < 1:
 print(sys.argv[1])
 
 with open(sys.argv[1], "r") as stream:
-    data_loaded = yaml.load(stream)
+    data_loaded = yaml.load(stream, Loader=yaml.FullLoader)
 
 
 for e in data_loaded:
