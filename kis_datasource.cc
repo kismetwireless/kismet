43b6ba7a509a99623b65b73773fadbd7a3e15b99
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2023-05-18 10:23:12 -0400

    datasource: More work on cleaning up datasources

--- kis_datasource.cc
+++ kis_datasource.cc
@@ -493,7 +493,9 @@ void kis_datasource::connect_remote(std::string in_definition, kis_datasource* i
    
     last_pong = (time_t) Globalreg::globalreg->last_tv_sec;
 
-    timetracker->remove_timer(ping_timer_id);
+    if (ping_timer_id > 0)
+        timetracker->remove_timer(ping_timer_id);
+
     ping_timer_id = timetracker->register_timer(std::chrono::seconds(5), true, [this](int) -> int {
         kis_lock_guard<kis_mutex> lk(ext_mutex, "datasource ping_timer lambda");
         
@@ -514,6 +516,10 @@ void kis_datasource::connect_remote(std::string in_definition, kis_datasource* i
 
     // Unlock before attaching sockets
     lk.unlock();
+
+    if (io_ != nullptr) {
+        io_->stop();
+    }
 
     // Inherit the incoming io mode
     io_ = in_remote->move_io(shared_from_this());
