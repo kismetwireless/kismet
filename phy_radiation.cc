b4bb2d8d663a5110115a8f1979c7583b0757485d
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2024-04-01 14:03:37 -0400

    radview: Initial test spectrum display code, basic spectrum aggregating

--- phy_radiation.cc
+++ phy_radiation.cc
@@ -22,12 +22,17 @@
 #include "phy_radiation.h"
 #include "devicetracker.h"
 #include "macaddr.h"
+#include "kis_httpd_registry.h"
 
 kis_radiation_phy::kis_radiation_phy(int in_phyid) :
     kis_phy_handler(in_phyid) {
 
     set_phy_name("RADIATION");
     indexed = false;
+
+    geiger_device_id = 
+        Globalreg::globalreg->entrytracker->register_field("radiation.geiger",
+                tracker_element_factory<geiger_device>(), "Geiger counter");
 
     packetchain =
         Globalreg::fetch_mandatory_global_as<packet_chain>();
@@ -40,8 +45,24 @@ kis_radiation_phy::kis_radiation_phy(int in_phyid) :
         packetchain->register_packet_component("JSON");
     pack_comp_meta =
         packetchain->register_packet_component("METABLOB");
+    pack_comp_datasrc =
+        packetchain->register_packet_component("KISDATASRC");
 
 	packetchain->register_handler(&packet_handler, this, CHAINPOS_CLASSIFIER, -100);
+
+    geiger_counters = 
+        std::make_shared<tracker_element_uuid_map>();
+
+    auto httpd = 
+        Globalreg::fetch_mandatory_global_as<kis_net_beast_httpd>();
+
+    auto httpregistry =
+        Globalreg::fetch_mandatory_global_as<kis_httpd_registry>();
+    httpregistry->register_js_module("kismet_ui_radiation", "js/kismet.ui.radiation.js");
+
+    httpd->register_route("/radiation/sensors/all_sensors", {"GET", "POST"},
+            httpd->RO_ROLE, {},
+            std::make_shared<kis_net_web_tracked_endpoint>(geiger_counters, rad_mutex));
 }
 
 kis_radiation_phy::~kis_radiation_phy() {
@@ -51,19 +72,69 @@ kis_radiation_phy::~kis_radiation_phy() {
 int kis_radiation_phy::packet_handler(CHAINCALL_PARMS) {
     kis_radiation_phy *radphy = (kis_radiation_phy *) auxdata;
 
-    if (in_pack->error || in_pack->filtered || in_pack->duplicate)
+    if (in_pack->error || in_pack->filtered || in_pack->duplicate) {
         return 0;
+    }
 
     auto json = in_pack->fetch<kis_json_packinfo>(radphy->pack_comp_json);
 
-    if (json == nullptr)
+    if (json == nullptr) {
         return 0;
+    }
 
+    auto datasrc = in_pack->fetch<packetchain_comp_datasource>(radphy->pack_comp_datasrc);
+
+    if (datasrc == nullptr) {
+        return 0;
+    }
+
+    // Radview arduino code emits the CPS and spectrum data
+    if (json->type == "radview") {
+
+        nlohmann::json json_;
+        std::vector<double> spectrum;
+        double cps;
+
+        try {
+            std::stringstream ss(json->json_string);
+            ss >> json_;
+
+            cps = json_["cps"].get<double>();
+            spectrum = json_["spectrum"].get<std::vector<double>>();
+
+        } catch (const std::exception& e) {
+            _MSG_DEBUG("JSON parsing error: {}", e.what());
+            return 0;
+        }
+
+        // _MSG_DEBUG("Radview JSON: {}", json->json_string);
+
+        auto lk = kis_lock_guard<kis_mutex>(radphy->rad_mutex, "radphy update");
+        std::shared_ptr<geiger_device> rv;
+        auto gk = radphy->geiger_counters->find(datasrc->ref_source->get_source_uuid());
+        if (gk == radphy->geiger_counters->end()) {
+            rv = std::make_shared<geiger_device>(radphy->geiger_device_id);
+            rv->set_detector_uuid(datasrc->ref_source->get_source_uuid());
+            rv->set_detector_type("Radview");
+            radphy->geiger_counters->insert(datasrc->ref_source->get_source_uuid(), rv);
+        } else {
+            rv = std::static_pointer_cast<geiger_device>(gk->second);
+        }
+
+        rv->insert_cps_record(in_pack->ts.tv_sec, cps, spectrum);
+
+        return 1;
+    }
+
+    /*
     if (json->type != "radiation")
         return 0;
 
     auto rdata = in_pack->fetch_or_add<packet_metablob>(radphy->pack_comp_meta);
     rdata->set_data("radiation", json->json_string);
+    */
+
+
 
     return 1;
 }
--- phy_radiation.h
+++ phy_radiation.h
@@ -23,6 +23,106 @@
 #include "config.h"
 #include "globalregistry.h"
 #include "phyhandler.h"
+#include "configfile.h"
+
+class geiger_device : public tracker_component {
+public:
+    geiger_device() :
+        tracker_component() {
+            register_fields();
+            reserve_fields(nullptr);
+
+            rolling_sz = 
+                Globalreg::globalreg->kismet_config->fetch_opt_uint("geiger_spectrum_history", 4096);
+        }
+
+    geiger_device(int in_id) :
+        tracker_component(in_id) {
+            register_fields();
+            reserve_fields(nullptr);
+
+            rolling_sz = 
+                Globalreg::globalreg->kismet_config->fetch_opt_uint("geiger_spectrum_history", 4096);
+        }
+
+    geiger_device(int in_id, std::shared_ptr<tracker_element_map> e) :
+        tracker_component(in_id) {
+            register_fields();
+            reserve_fields(e);
+
+            rolling_sz = 
+                Globalreg::globalreg->kismet_config->fetch_opt_uint("geiger_spectrum_history", 4096);
+        }
+
+    geiger_device(const geiger_device *p):
+        tracker_component(p) {
+            rolling_sz = p->rolling_sz;
+
+            __ImportField(aggregate_spectrum, p);
+            __ImportField(detector_uuid, p);
+            __ImportField(cps_rrd, p);
+            __ImportField(usv_rrd, p);
+        }
+
+    void insert_cps_record(time_t ts, double cps, const std::vector<double>& spectrum) {
+        cps_rrd->add_sample(cps, ts);
+
+        std::vector<double> del_vec;
+        bool del = false;
+
+        if (aggregate_spectrum->size() == 0) {
+            aggregate_spectrum->set(spectrum);
+            spectrum_rolling_log.push_back(spectrum);
+            return;
+        }
+
+        if (spectrum_rolling_log.size() >= rolling_sz) {
+            del = true;
+            del_vec = spectrum_rolling_log.front();
+            spectrum_rolling_log.pop_front();
+        }
+        spectrum_rolling_log.push_back(spectrum);
+
+        for (unsigned int x = 0; x < spectrum.size(); x++) {
+            if (x >= aggregate_spectrum->size()) {
+                break;
+            }
+
+            if (del && x >= del_vec.size()) {
+                break;
+            }
+        
+            // Add new, remove last if it rolls off the back
+            (*aggregate_spectrum)[x] += spectrum[x] - (del ? del_vec[x] : 0);
+        }
+
+    }
+
+    __Proxy(detector_uuid, uuid, uuid, uuid, detector_uuid);
+    __Proxy(detector_type, std::string, std::string, std::string, detector_type);
+
+protected:
+    virtual void register_fields() override {
+        tracker_component::register_fields();
+
+        register_field("radiation.sensor.aggregate_spectrum", "Aggregated spectrum over time", &aggregate_spectrum);
+        register_field("radiation.sensor.uuid", "Detector UUID", &detector_uuid);
+        register_field("radiation.sensor.cps_rrd", "Counts-per-second RRD", &cps_rrd);
+        register_field("radiation.sensor.usv_rrd", "uSV dosage RRD (if available)", &usv_rrd);
+        register_field("radiation.sensor.type", "Detector type/brand", &detector_type);
+
+    }
+
+    std::shared_ptr<tracker_element_vector_double> aggregate_spectrum;
+    std::shared_ptr<tracker_element_uuid> detector_uuid;
+    std::shared_ptr<tracker_element_string> detector_type;
+    std::shared_ptr<kis_tracked_rrd<kis_tracked_rrd_extreme_aggregator>> cps_rrd;
+    std::shared_ptr<kis_tracked_rrd<kis_tracked_rrd_extreme_aggregator>> usv_rrd;
+
+    std::list<std::vector<double>> spectrum_rolling_log;
+
+    unsigned int rolling_sz;
+};
 
 class kis_radiation_phy : public kis_phy_handler {
 public:
@@ -42,10 +142,17 @@ public:
     static int packet_handler(CHAINCALL_PARMS);
 
 protected:
+    kis_mutex rad_mutex;
+
     std::shared_ptr<packet_chain> packetchain;
     std::shared_ptr<device_tracker> devicetracker;
 
-    int pack_comp_common, pack_comp_json, pack_comp_meta, pack_comp_radiodata;
+    std::shared_ptr<tracker_element_uuid_map> geiger_counters;
+
+    int pack_comp_common, pack_comp_json, pack_comp_meta, pack_comp_radiodata, 
+        pack_comp_datasrc;
+
+    uint16_t geiger_device_id;
 };
 
 
