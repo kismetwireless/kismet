12f530703506c07b8fd10bd4067bd450fc24a146
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2024-04-19 15:06:00 -0400

    bluetooth: Work towards modernizing bluetooth types

--- bluetooth_types.h
+++ bluetooth_types.h
@@ -24,18 +24,48 @@
 #include <string>
 #include <unordered_map>
 
-extern std::unordered_map<unsigned int, const char *> bt_major_dev_class;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_computer;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_phone;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_lan_load;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_av;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_peripheral;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_type_peripheral;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_wearable;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_toy;
-extern std::unordered_map<unsigned int, const char *> bt_minor_dev_class_health;
-extern std::unordered_map<unsigned int, const char *> bt_appearance;
-extern std::unordered_map<unsigned int, const char *> bt_io_capability;
+#include "globalregistry.h"
+#include "trackedcomponent.h"
+
+// Very basic mapped class to map the (large number) of bt type
+// IDs to human strings.  
+//
+// Major and minor classes are stored as pre-typed string elements
+// since these are known types for the device tracker elements.
+
+class kis_bt_types : public lifetime_global {
+public:
+    static std::string global_name() { return "BT_TYPES"; }
+
+    static std::shared_ptr<kis_bt_types> create_bt_types() {
+        std::shared_ptr<kis_bt_types> r(new kis_bt_types());
+        Globalreg::globalreg->register_lifetime_global(r);
+        Globalreg::globalreg->insert_global(global_name(), r);
+        return r;
+    }
+
+    ~kis_bt_types();
+
+private:
+    kis_bt_types();
+
+public:
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_major_dev_class;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_computer;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_phone;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_lan_load;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_av;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_peripheral;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_type_peripheral;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_wearable;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_toy;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_minor_dev_class_health;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_appearance;
+    std::unordered_map<unsigned int, std::shared_ptr<tracker_element_string> > bt_io_capability;
+
+protected:
+    int major_class_id, minor_class_id;
+};
 
 #endif
 
