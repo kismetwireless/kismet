566b57c7ab538e36e31fd0732305cc39799d644a
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2025-03-13 09:04:15 -0400

    linuxhci: try to restore old protobuf remote functionality

--- datasource_linux_bluetooth.cc
+++ datasource_linux_bluetooth.cc
@@ -20,6 +20,12 @@
 
 #include "datasource_linux_bluetooth.h"
 
+#ifdef HAVE_PROTOBUF_CPP
+#include "kis_gps.h"
+#include "json_adapter.h"
+#include "protobuf_cpp/linuxbluetooth.pb.h"
+#endif
+
 #ifdef HAVE_LINUX_BLUETOOTH_DATASOURCE
 
 kis_datasource_linux_bluetooth::kis_datasource_linux_bluetooth(shared_datasource_builder in_builder) :
@@ -28,7 +34,7 @@ kis_datasource_linux_bluetooth::kis_datasource_linux_bluetooth(shared_datasource
     set_int_source_ipc_binary("kismet_cap_linux_bluetooth");
 }
 
-#if 0
+#ifdef HAVE_PROTOBUF_CPP
 bool kis_datasource_linux_bluetooth::dispatch_rx_packet(const nonstd::string_view& command,
         uint32_t seqno, const nonstd::string_view& content) {
     if (kis_datasource::dispatch_rx_packet(command, seqno, content))
@@ -42,12 +48,12 @@ bool kis_datasource_linux_bluetooth::dispatch_rx_packet(const nonstd::string_vie
     return false;
 }
 
-void kis_datasource_linux_bluetooth::handle_packet_linuxbtdevice(uint32_t in_seqno, 
+void kis_datasource_linux_bluetooth::handle_packet_linuxbtdevice(uint32_t in_seqno,
         const nonstd::string_view& in_content) {
 
     // If we're paused, throw away this packet
     {
-        kis_lock_guard<kis_mutex> lk(ext_mutex, 
+        kis_lock_guard<kis_mutex> lk(ext_mutex,
                 "kis_datasource_linux_bluetooth handle_packet_linuxbtdevice");
 
         if (get_source_paused())
@@ -57,14 +63,14 @@ void kis_datasource_linux_bluetooth::handle_packet_linuxbtdevice(uint32_t in_seq
     KismetLinuxBluetooth::LinuxBluetoothDataReport report;
 
     if (!report.ParseFromArray(in_content.data(), in_content.length())) {
-        _MSG(std::string("Kismet datasource driver ") + get_source_builder()->get_source_type() + 
+        _MSG(std::string("Kismet datasource driver ") + get_source_builder()->get_source_type() +
                 std::string(" could not parse the data report, something is wrong with "
                     "the remote capture tool"), MSGFLAG_ERROR);
         trigger_error("Invalid LBTDATAREPORT");
         return;
     }
 
-    if (report.has_message()) 
+    if (report.has_message())
         _MSG(report.message().msgtext(), report.message().msgtype());
 
     if (report.has_warning())
@@ -74,10 +80,6 @@ void kis_datasource_linux_bluetooth::handle_packet_linuxbtdevice(uint32_t in_seq
         gpstracker = Globalreg::fetch_mandatory_global_as<gps_tracker>();
 
     auto packet = packetchain->generate_packet();
-
-    auto bpi = packetchain->new_packet_component<bluetooth_packinfo>();
-
-    packet->insert(pack_comp_btdevice, bpi);
 
     if (report.has_signal()) {
         auto siginfo = handle_sub_signal(report.signal());
@@ -110,37 +112,17 @@ void kis_datasource_linux_bluetooth::handle_packet_linuxbtdevice(uint32_t in_seq
         packet->ts.tv_usec = report.btdevice().time_usec();
     }
 
-    bpi->address = mac_addr(report.btdevice().address());
-    bpi->name = munge_to_printable(report.btdevice().name());
-    bpi->txpower = report.btdevice().txpower();
-    bpi->type = report.btdevice().type();
-
-    for (auto u : report.btdevice().uuid_list()) 
-        bpi->service_uuid_vec.push_back(uuid(u));
-
-    // Forge a metablob until we transition the capture protocols
     std::stringstream fake_json;
     fake_json << "{";
-    fake_json << "\"bt_address\":\"" << bpi->address << "\",";
-    fake_json << "\"bt_name\":\"" << json_adapter::sanitize_string(bpi->name) << "\",";
-    fake_json << "\"txpower\":" << bpi->txpower << ",";
-    fake_json << "\"type\":" << bpi->type << ",";
-    fake_json << "\"uuid_list\": [";
-
-    bool need_comma = false;
-    for (auto u : bpi->service_uuid_vec) {
-        fake_json << "\"" << u << "\"";
-        if (need_comma) 
-            fake_json << ",";
-        need_comma = true;
-    }
-
-    fake_json << "]";
+    fake_json << "\"addr\": \"" << mac_addr(report.btdevice().address()) << "\",";
+    fake_json << "\"name\": \"" << json_adapter::sanitize_string(report.btdevice().name()) << "\",";
+    fake_json << "\"type\": " << report.btdevice().type();
     fake_json << "}";
 
-    auto metablob = packetchain->new_packet_component<packet_metablob>();
-    metablob->set_data("LINUXBLUETOOTH", fake_json.str());
-    packet->insert(pack_comp_meta, metablob);
+    auto jsonblob = packetchain->new_packet_component<kis_json_packinfo>();
+    jsonblob->type = "linuxbthci";
+    jsonblob->json_string = fake_json.str();
+    packet->insert(pack_comp_json, jsonblob);
 
     auto datasrcinfo = packetchain->new_packet_component<packetchain_comp_datasource>();
     datasrcinfo->ref_source = this;
